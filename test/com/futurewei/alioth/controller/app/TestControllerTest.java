257e3612a70afc772dca44f1d5bde26ea661271c
Liguang Xie <lxie@futurewei.com>
2019-07-22 13:16:12 -0700

    Create initial API Documentation with Restdocs

--- test/com/futurewei/alioth/controller/app/TestControllerTest.java
+++ test/com/futurewei/alioth/controller/app/TestControllerTest.java
@@ -1,22 +1,26 @@
 package com.futurewei.alioth.controller.app;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
 import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(TestController.class)
+//@SpringBootTest
+@AutoConfigureRestDocs(outputDir = "target/snippets")
 public class TestControllerTest {
 
     @Autowired
@@ -24,8 +28,10 @@ public class TestControllerTest {
 
     @Test
     public void shouldReturnDefaultMessage() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+        this.mvc.perform(get("/"))
+                .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("Greetings from Alioth!")));
+                .andExpect(content().string(equalTo("Greetings from Alioth")))
+                .andDo(document("home"));
     }
 }
