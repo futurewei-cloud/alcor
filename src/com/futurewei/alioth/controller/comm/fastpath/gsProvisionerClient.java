d7398d6f11896e89afb50eef580bea9a6265f6c4
xieus <liguang.vt@gmail.com>
2019-08-16 22:43:04 -0700

    Add gRPC dependencies in pom file

---
+++ src/com/futurewei/alioth/controller/comm/fastpath/gsProvisionerClient.java
@@ -0,0 +1,11 @@
+package com.futurewei.alioth.controller.comm.fastpath;
+
+import com.futurewei.alioth.controller.service.Goalstateprovisioner;
+import io.grpc.ManagedChannel;
+
+public class gsProvisionerClient {
+
+    //private final ManagedChannel channel;
+    //private final Goalstateprovisioner
+
+}
