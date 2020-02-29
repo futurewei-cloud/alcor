277b709c11af1695262ce96ee1d63a94a2d3764a
chenpiaoping <chenpiaoping@yeah.net>
2020-02-29 17:53:20 +0800

    Add ignite cache

---
+++ src/com/futurewei/alcor/controller/cache/repo/VpcIgniteRepository.java
@@ -0,0 +1,64 @@
+package com.futurewei.alcor.controller.cache.repo;
+
+import com.futurewei.alcor.controller.logging.Logger;
+import com.futurewei.alcor.controller.logging.LoggerFactory;
+import com.futurewei.alcor.controller.model.VpcState;
+import org.apache.ignite.client.ClientCache;
+import org.apache.ignite.client.ClientException;
+import org.apache.ignite.client.IgniteClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.Assert;
+import javax.annotation.PostConstruct;
+import java.util.Map;
+import java.util.logging.Level;
+
+@Repository
+public class VpcIgniteRepository implements ICacheRepository<VpcState> {
+    private static final Logger logger = LoggerFactory.getLogger();
+    private static final String KEY = "VpcState";
+
+    private ClientCache<String, VpcState> cache;
+
+    @Autowired
+    public VpcIgniteRepository(IgniteClient igniteClient) {
+        try {
+            cache = igniteClient.getOrCreateCache(KEY);
+        }
+        catch (ClientException e) {
+            logger.log(Level.WARNING, "Create cache for vpc failed:" + e.getMessage());
+        }
+        catch (Exception e) {
+            logger.log(Level.WARNING, "Unexpected failure:" + e.getMessage());
+        }
+
+        Assert.notNull(igniteClient, "Create cache for vpc failed");
+    }
+
+    @PostConstruct
+    private void init() {
+
+    }
+
+    @Override
+    public VpcState findItem(String id) {
+        return (VpcState) cache.get(id);
+    }
+
+    @Override
+    public Map findAllItems() {
+        return null;
+    }
+
+    @Override
+    public void addItem(VpcState vpcState) {
+        logger.log(Level.INFO, "Add vpc, Vpc Id:" + vpcState.getId());
+        cache.put(vpcState.getId(), vpcState);
+    }
+
+    @Override
+    public void deleteItem(String id) {
+        logger.log(Level.INFO, "Delete vpc, Vpc Id:" + id);
+        cache.remove(id);
+    }
+}
---
+++ src/com/futurewei/alcor/controller/web/DebugVpcController.java
@@ -0,0 +1,137 @@
+package com.futurewei.alcor.controller.web;
+
+import com.futurewei.alcor.controller.cache.repo.VpcIgniteRepository;
+import com.futurewei.alcor.controller.exception.ParameterNullOrEmptyException;
+import com.futurewei.alcor.controller.exception.ResourceNotFoundException;
+import com.futurewei.alcor.controller.exception.ResourceNullException;
+import com.futurewei.alcor.controller.exception.ResourcePersistenceException;
+import com.futurewei.alcor.controller.model.ResponseId;
+import com.futurewei.alcor.controller.model.VpcState;
+import com.futurewei.alcor.controller.model.VpcStateJson;
+import com.futurewei.alcor.controller.web.util.RestPreconditions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
+
+@RestController
+public class DebugVpcController {
+    @Autowired
+    private VpcIgniteRepository vpcIgniteRepository;
+
+    @RequestMapping(
+            method = GET,
+            value = {"/debug/project/{projectid}/vpcs/{vpcid}"})
+    public VpcStateJson getVpcStateByVpcId(@PathVariable String projectid, @PathVariable String vpcid) throws Exception {
+
+        VpcState vpcState = null;
+
+        try {
+            RestPreconditions.verifyParameterNotNullorEmpty(projectid);
+            RestPreconditions.verifyParameterNotNullorEmpty(vpcid);
+            RestPreconditions.verifyResourceFound(projectid);
+
+            vpcState = this.vpcIgniteRepository.findItem(vpcid);
+        } catch (ParameterNullOrEmptyException e) {
+            //TODO: REST error code
+            throw new Exception(e);
+        }
+
+        if (vpcState == null) {
+            //TODO: REST error code
+            return new VpcStateJson();
+        }
+
+        return new VpcStateJson(vpcState);
+    }
+
+    @RequestMapping(
+            method = POST,
+            value = {"/debug/project/{projectid}/vpcs"})
+    @ResponseStatus(HttpStatus.CREATED)
+    public VpcStateJson createVpcState(@PathVariable String projectid, @RequestBody VpcStateJson resource) throws Exception {
+        VpcState vpcState = null;
+
+        try {
+            RestPreconditions.verifyParameterNotNullorEmpty(projectid);
+
+            VpcState inVpcState = resource.getVpc();
+            RestPreconditions.verifyResourceNotNull(inVpcState);
+            RestPreconditions.populateResourceProjectId(inVpcState, projectid);
+
+            this.vpcIgniteRepository.addItem(inVpcState);
+
+            vpcState = this.vpcIgniteRepository.findItem(inVpcState.getId());
+            if (vpcState == null) {
+                throw new ResourcePersistenceException();
+            }
+        } catch (ParameterNullOrEmptyException e) {
+            throw new Exception(e);
+        } catch (ResourceNullException e) {
+            throw new Exception(e);
+        }
+
+        return new VpcStateJson(vpcState);
+    }
+
+    @RequestMapping(
+            method = PUT,
+            value = {"/debug/project/{projectid}/vpcs/{vpcid}"})
+    public VpcStateJson updateVpcStateByVpcId(@PathVariable String projectid, @PathVariable String vpcid, @RequestBody VpcStateJson resource) throws Exception {
+
+        VpcState vpcState = null;
+
+        try {
+            RestPreconditions.verifyParameterNotNullorEmpty(projectid);
+            RestPreconditions.verifyParameterNotNullorEmpty(vpcid);
+
+            VpcState inVpcState = resource.getVpc();
+            RestPreconditions.verifyResourceNotNull(inVpcState);
+            RestPreconditions.populateResourceProjectId(inVpcState, projectid);
+            RestPreconditions.populateResourceVpcId(inVpcState, vpcid);
+
+            vpcState = this.vpcIgniteRepository.findItem(vpcid);
+            if (vpcState == null) {
+                throw new ResourceNotFoundException("Vpc not found : " + vpcid);
+            }
+
+            this.vpcIgniteRepository.addItem(inVpcState);
+
+            vpcState = this.vpcIgniteRepository.findItem(vpcid);
+
+        } catch (ParameterNullOrEmptyException e) {
+            throw new Exception(e);
+        }
+
+        return new VpcStateJson(vpcState);
+    }
+
+    @RequestMapping(
+            method = DELETE,
+            value = {"/debug/project/{projectid}/vpcs/{vpcid}"})
+    public ResponseId deleteVpcStateByVpcId(@PathVariable String projectid, @PathVariable String vpcid) throws Exception {
+        VpcState vpcState = null;
+
+        try {
+            RestPreconditions.verifyParameterNotNullorEmpty(projectid);
+            RestPreconditions.verifyParameterNotNullorEmpty(vpcid);
+            RestPreconditions.verifyResourceFound(projectid);
+
+            vpcState = this.vpcIgniteRepository.findItem(vpcid);
+            if (vpcState == null) {
+                return new ResponseId();
+            }
+
+            vpcIgniteRepository.deleteItem(vpcid);
+        } catch (ParameterNullOrEmptyException e) {
+            throw new Exception(e);
+        }
+
+        return new ResponseId(vpcid);
+    }
+}
