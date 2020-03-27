f994b2195dbc1ebdefddd4df80298e91b48c5546
chenpiaoping <chenpiaoping@yeah.net>
2020-03-27 16:23:55 +0800

    add one-box deployment script and k8s yaml file for ignite

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,20 @@
 
 CtrlContainer="alcor-controller"
 RedisContainer="alcor-redis"
+IgniteContainer="alcor-ignite"
+IgniteImage="apacheignite/ignite:2.8.0"
+DbType="redis"
+
+# Parse all params
+while [ $# -gt 0 ];
+do
+   case $1 in
+   --db-type) DbType=$2
+       shift
+       ;;
+   esac
+   shift
+done
 
 # Install Docker
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
@@ -31,20 +45,43 @@ else
     echo "Unknown supported OS"
 fi
 
-# Deploy a redis container
-if [ -n "$(docker ps -q -f name=$RedisContainer)" ]; then
-    echo "Redis container $RedisContainer is already running. Build exits...\n"
-    exit 1
-else
-    if [ -n "$(docker ps -aq -f status=exited -f name=$RedisContainer)" ]; then
-        echo "Clean up non-running redis container\n"
-        docker rm $RedisContainer
-    else
-        echo "Doesn't detect any matching container. Proceed...\n"
-    fi
+echo "Database type is $DbType"
 
-    echo "Deploy a redis container\n"
-    docker run -p 6379:6379 --name $RedisContainer -d redis
+if [[ "$DbType" == "ignite" ]]; then
+  # Deploy a ignite container
+  if [ -n "$(docker ps -q -f name=$IgniteContainer)" ]; then
+      echo "Ignite container $IgniteContainer is already running. Build exits..."
+      exit 1
+  else
+      if [ -n "$(docker ps -aq -f status=exited -f name=$IgniteContainer)" ]; then
+          echo "Clean up non-running ignite container"
+          docker rm $IgniteContainer
+      else
+          echo "Doesn't detect any matching container. Proceed..."
+      fi
+
+      echo "Deploy a ignite container"
+      docker run -p 10800:10800 --name $IgniteContainer -d $IgniteImage
+  fi
+elif [[ "$DbType" == "redis" ]]; then
+  # Deploy a redis container
+  if [ -n "$(docker ps -q -f name=$RedisContainer)" ]; then
+      echo "Redis container $RedisContainer is already running. Build exits...\n"
+      exit 1
+  else
+      if [ -n "$(docker ps -aq -f status=exited -f name=$RedisContainer)" ]; then
+          echo "Clean up non-running redis container\n"
+          docker rm $RedisContainer
+      else
+          echo "Doesn't detect any matching container. Proceed...\n"
+      fi
+
+      echo "Deploy a redis container\n"
+      docker run -p 6379:6379 --name $RedisContainer -d redis
+  fi
+else
+  echo "Unknown database type: $DbType"
+  exit 1
 fi
 
 # Build a controller image and deploy as a docker container
