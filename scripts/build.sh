2de89fa303c4515e9bd5833252e5b1bd7ddc7b51
Zhonghao(Kevin) Lyu <52331056+kevin-zhonghao@users.noreply.github.com>
2020-03-27 12:00:13 -0700

    VPC Manager service module (#134)
    * split vpc manager service from alcor
    
    * re-organize vpc manager directory structure
    
    * extract common library shared by multiple microservices
    
    * update kubernetes files in vpc manager
    
    Co-authored-by: Liguang Xie <lxie@futurewei.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,30 +2,30 @@
 
 # Install prerequisites
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    echo "Install prerequisites in Linux OS"
-    sudo apt-get update
-    sudo apt-get install maven
-    sudo apt-get install openjdk-8-jdk
+  echo "Install prerequisites in Linux OS"
+  sudo apt-get update
+  sudo apt-get install maven
+  sudo apt-get install openjdk-8-jdk
 
-    # Clean build controller project
-    mvn clean
-    mvn compile
-    mvn install
+  # Clean build controller project
+  mvn clean
+  mvn compile
+  mvn install
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    echo "Install prerequisites in Mac OSX"
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    brew install maven
+  echo "Install prerequisites in Mac OSX"
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew install maven
 
-    # Clean build controller project
-    mvn clean
-    mvn compile
-    mvn install -DskipTests
+  # Clean build controller project
+  mvn clean
+  mvn compile
+  mvn install -DskipTests
 elif [[ "$OSTYPE" == "cygwin" ]]; then
-    # POSIX compatibility layer and Linux environment emulation for Windows
-    echo "Install prerequisites in Linux environment of Windows"
+  # POSIX compatibility layer and Linux environment emulation for Windows
+  echo "Install prerequisites in Linux environment of Windows"
 elif [[ "$OSTYPE" == "msys" ]]; then
-    # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
-    echo "Install prerequisites in MinGW of Windows"
+  # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+  echo "Install prerequisites in MinGW of Windows"
 else
-    echo "Unknown supported OS"
+  echo "Unknown supported OS"
 fi
