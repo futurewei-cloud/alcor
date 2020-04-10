5886a7797013af2b20a41104926ab5946a98e8ac
Eunju Kim <ekim@futurewei.com>
2020-04-09 19:21:27 -0700

    [Microservices] Mac manager - updated unit test cases

--- services/mac_manager/src/test/java/com/futurewei/alcor/macmanager/controller/MacControllerTest.java
+++ services/mac_manager/src/test/java/com/futurewei/alcor/macmanager/controller/MacControllerTest.java
@@ -15,35 +15,98 @@ Licensed under the Apache License, Version 2.0 (the "License");
 */
 package com.futurewei.alcor.macmanager.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.futurewei.alcor.macmanager.entity.MacState;
+import com.futurewei.alcor.macmanager.entity.MacStateJson;
+import com.futurewei.alcor.macmanager.service.MacAddressService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.equalTo;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
 @RunWith(SpringRunner.class)
-@WebMvcTest(MacController.class)
-@ContextConfiguration(classes = {MacController.class})
+@SpringBootTest
+@AutoConfigureMockMvc
 public class MacControllerTest {
+
+    private static final ObjectMapper om = new ObjectMapper();
+
+    public MacState testMacState;
     @Autowired
-    private MockMvc mvc;
+    MacAddressService service;
+    String strTestMac = "";
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private MacController mockController;
+
+    @Before
+    public void init() {
+        MacState macState = new MacState("", "project1", "vpc1", "port1");
+        MacStateJson macStateJson = new MacStateJson(macState);
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            String json = objectMapper.writeValueAsString(macStateJson);
+            MacState macState2 = service.createMacState(macState);
+            strTestMac = macState2.getMacAddress();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     @Test
     public void test_index() throws Exception {
-        this.mvc.perform(get("/start.html"))
+        this.mockMvc.perform(get("/start.html"))
                 .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().string(containsString("MacManager")));
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void test_createMacState() throws Exception {
+        MacState macState = new MacState("", "project1", "vpc1", "port2");
+
+        MacStateJson macStateJson = new MacStateJson(macState);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(macStateJson);
+
+        System.out.println(json);
+        MvcResult mvcResult = this.mockMvc.perform(post("/macs")
+                .content(json)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andDo(print())
+                .andReturn();
+    }
+
+    @Test
+    public void test_getMacStateByMacAddress() throws Exception {
+        init();
+        this.mockMvc.perform(get("/macs/" + strTestMac))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void test_deleteMacStateByMacAddress() throws Exception {
+        init();
+        this.mockMvc.perform(delete("/macs/" + strTestMac))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 }
+
+
