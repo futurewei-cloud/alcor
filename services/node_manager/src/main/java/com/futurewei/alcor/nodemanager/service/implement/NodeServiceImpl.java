ec24cf46fd84c7d5f1816447240b7f8dcf9c31bd
Eunju Kim <ekim@futurewei.com>
2020-04-28 06:31:48 -0700

    Microservices] Node Manager - implemented UT and tested APIs.

--- services/node_manager/src/main/java/com/futurewei/alcor/nodemanager/service/implement/NodeServiceImpl.java
+++ services/node_manager/src/main/java/com/futurewei/alcor/nodemanager/service/implement/NodeServiceImpl.java
@@ -15,10 +15,11 @@ Licensed under the Apache License, Version 2.0 (the "License");
 package com.futurewei.alcor.nodemanager.service.implement;
 
 import com.futurewei.alcor.common.exception.ParameterNullOrEmptyException;
-import com.futurewei.alcor.nodemanager.dao.file.DataCenterConfigLoader;
-import com.futurewei.alcor.nodemanager.dao.repository.NodeRepository;
+import com.futurewei.alcor.nodemanager.service.datacenter.DataCenterConfigLoader;
+import com.futurewei.alcor.nodemanager.dao.NodeRepository;
 import com.futurewei.alcor.nodemanager.entity.NodeInfo;
 import com.futurewei.alcor.nodemanager.service.NodeService;
+import com.futurewei.alcor.nodemanager.service.datacenter.NodeManager;
 import com.futurewei.alcor.nodemanager.utils.NodeUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,38 +27,93 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.List;
 
 @Service
 public class NodeServiceImpl implements NodeService {
     private static final Logger logger = LoggerFactory.getLogger(NodeServiceImpl.class);
-    static public Hashtable<String, NodeInfo> Nodes = new Hashtable<String, NodeInfo>();
+    public Hashtable<String, NodeInfo> Nodes = new Hashtable<String, NodeInfo>();
 
     @Value("${nodemanager.nodeinfo.location}")
-    private String nodeInfoLocation;
+    private int nodeInfoLocation;
 
     @Autowired
     private NodeRepository nodeRepository;
 
     @Autowired
-    private DataCenterConfigLoader dataCenterConfigLoader;
+    private NodeManager nodeManager;
 
     @Override
-    public NodeInfo getNodeInfoById(String hostId) throws Exception {
+    public NodeInfo getNodeInfoById(String nodeId) throws Exception {
         NodeInfo nodeInfo = null;
         try {
-            nodeInfo = nodeRepository.findItem(hostId);
+            switch(nodeInfoLocation){
+                case NodeUtil.NODE_INFO_FILE: {
+                    nodeInfo = nodeManager.getNodeInfoById(nodeId);
+                }
+                case NodeUtil.NODE_INFO_REPOSITOTY: {
+                    nodeInfo = nodeRepository.findItem(nodeId);
+                }
+            }
         } catch (Exception e) {
             throw e;
         }
         return nodeInfo;
+    }
+
+    @Override
+    public Hashtable<String, NodeInfo> getAllNodes() throws Exception {
+        Hashtable<String, NodeInfo> nodes = new Hashtable<String, NodeInfo>();
+        try {
+            switch(nodeInfoLocation){
+                case NodeUtil.NODE_INFO_FILE: {
+                    nodes = nodeManager.getAllNodes();
+                    break;
+                }
+                case NodeUtil.NODE_INFO_REPOSITOTY: {
+                    nodes.putAll(nodeRepository.findAllItems());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            throw e;
+        }
+        return nodes;
+    }
+
+    public List<NodeInfo> getAllNodesList() throws Exception {
+        List<NodeInfo> nodes = new ArrayList<NodeInfo>();
+        try {
+            switch(nodeInfoLocation){
+                case NodeUtil.NODE_INFO_FILE: {
+                    nodes = nodeManager.getAllNodesList();
+                    break;
+                }
+                case NodeUtil.NODE_INFO_REPOSITOTY: {
+                    nodes = new ArrayList(nodeRepository.findAllItems().values());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            throw e;
+        }
+        return nodes;
     }
 
     @Override
     public NodeInfo createNodeInfo(NodeInfo nodeInfo) throws Exception {
         if (nodeInfo != null) {
             try {
-                nodeRepository.addItem(nodeInfo);
+                switch(nodeInfoLocation){
+                    case NodeUtil.NODE_INFO_FILE: {
+                        nodeManager.putNode(nodeInfo);
+                    }
+                    case NodeUtil.NODE_INFO_REPOSITOTY: {
+                        nodeRepository.addItem(nodeInfo);
+                    }
+                }
             } catch (Exception e) {
                 throw e;
             }
@@ -66,10 +122,17 @@ public class NodeServiceImpl implements NodeService {
     }
 
     @Override
-    public NodeInfo updateNodeInfo(String hostId, NodeInfo nodeInfo) throws Exception {
+    public NodeInfo updateNodeInfo(String nodeId, NodeInfo nodeInfo) throws Exception {
         if (nodeInfo != null) {
             try {
-                nodeRepository.addItem(nodeInfo);
+                switch(nodeInfoLocation){
+                    case NodeUtil.NODE_INFO_FILE: {
+                        nodeManager.putNode(nodeInfo);
+                    }
+                    case NodeUtil.NODE_INFO_REPOSITOTY: {
+                        nodeRepository.addItem(nodeInfo);
+                    }
+                }
             } catch (Exception e) {
                 throw e;
             }
@@ -78,14 +141,21 @@ public class NodeServiceImpl implements NodeService {
     }
 
     @Override
-    public String deleteNodeInfo(String hostId) throws Exception {
-        if (hostId == null)
+    public String deleteNodeInfo(String nodeId) throws Exception {
+        if (nodeId == null)
             throw (new ParameterNullOrEmptyException(NodeUtil.NODE_EXCEPTION_PARAMETER_NULL_EMPTY));
         try {
-            nodeRepository.deleteItem(hostId);
+            switch(nodeInfoLocation){
+                case NodeUtil.NODE_INFO_FILE: {
+                    nodeManager.deleteNode(nodeId);
+                }
+                case NodeUtil.NODE_INFO_REPOSITOTY: {
+                    nodeRepository.deleteItem(nodeId);
+                }
+            }
         } catch (Exception e) {
             throw e;
         }
-        return hostId;
+        return nodeId;
     }
 }
