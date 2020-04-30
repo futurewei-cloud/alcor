1c1ea01c2a4d5602d0e665c7e523342f9abdb13c
skdong <shikaidong@126.com>
2020-04-30 18:04:38 +0800

    add auto swagger not use mvn plugin

---
+++ services/vpc_manager/src/test/java/com/futurewei/alcor/vpcmanager/swagger/SwaggerConfig.java
@@ -0,0 +1,52 @@
+/*
+Copyright 2019 The Alcor Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.
+*/
+
+package com.futurewei.alcor.vpcmanager.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig{
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+            .apiInfo(apiInfo())
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.regex("(?!/error).+")):web
+            .paths(PathSelectors.regex("(?!/actuator).+"))
+            .build();
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+            .title("VPC Manager")
+            .description("VPC Manager")
+            .license("Apache 2.0")
+            .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+            .build();
+    }
+}
