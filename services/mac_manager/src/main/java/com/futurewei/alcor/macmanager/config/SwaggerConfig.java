8e67eab0e9220d93a49cf9604dcfff9ccaacc72f
Eunju Kim <ekim@futurewei.com>
2020-05-13 19:37:18 -0700

    [Micro services] MAC manager - moved swagger config to config package

--- services/mac_manager/src/main/java/com/futurewei/alcor/macmanager/config/SwaggerConfig.java
+++ services/mac_manager/src/main/java/com/futurewei/alcor/macmanager/config/SwaggerConfig.java
@@ -11,39 +11,38 @@ Licensed under the Apache License, Version 2.0 (the "License");
         limitations under the License.
 */
 
-        package com.futurewei.alcor.macmanager.config;
+package com.futurewei.alcor.macmanager.config;
 
-        import org.springframework.context.annotation.Bean;
-        import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-        import springfox.documentation.builders.PathSelectors;
-        import springfox.documentation.builders.RequestHandlerSelectors;
-        import springfox.documentation.service.ApiInfo;
-        import springfox.documentation.spi.DocumentationType;
-        import springfox.documentation.spring.web.plugins.Docket;
-        import springfox.documentation.builders.ApiInfoBuilder;
-        import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-        @Configuration
-        @EnableSwagger2
-        public class SwaggerConfig{
-        @Bean
-        public Docket api(){
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-        .apiInfo(apiInfo())
-        .select()
-        .apis(RequestHandlerSelectors.any())
-        .paths(PathSelectors.regex("(?!/error).+"))
-        .paths(PathSelectors.regex("(?!/actuator).+"))
-        .build();
-        }
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.regex("(?!/error).+"))
+                .paths(PathSelectors.regex("(?!/actuator).+"))
+                .build();
+    }
 
-        private ApiInfo apiInfo(){
+    private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-        .title("Virtual MAC Manager")
-        .description("Virtual MAC pool management")
-        .license("Apache 2.0")
-        .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
-        .build();
-        }
-        }
+                .title("Virtual MAC Manager")
+                .description("Virtual MAC pool management")
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .build();
+    }
+}
