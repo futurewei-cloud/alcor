be13f133e5a96c33c281703a32cd5c9b94472fc7
Eunju Kim <ekim@futurewei.com>
2020-05-17 02:15:17 -0700

    [Micro services] MAC manager - Updtae UTs with mockito

--- services/mac_manager/src/test/java/com/futurewei/alcor/macmanager/AlcorMacManager/MacManagerApplicationTests.java
+++ services/mac_manager/src/test/java/com/futurewei/alcor/macmanager/AlcorMacManager/MacManagerApplicationTests.java
@@ -15,7 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
 class MacManagerApplicationTests {
 
--- services/mac_manager/src/test/java/com/futurewei/alcor/macmanager/controller/MacControllerTest.java
+++ services/mac_manager/src/test/java/com/futurewei/alcor/macmanager/controller/MacControllerTest.java
@@ -16,36 +16,40 @@ Licensed under the Apache License, Version 2.0 (the "License");
 package com.futurewei.alcor.macmanager.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.futurewei.alcor.common.db.ignite.MockIgniteServer;
+import com.futurewei.alcor.common.entity.ResponseId;
 import com.futurewei.alcor.macmanager.entity.MacRange;
 import com.futurewei.alcor.macmanager.entity.MacRangeJson;
 import com.futurewei.alcor.macmanager.entity.MacState;
 import com.futurewei.alcor.macmanager.entity.MacStateJson;
 import com.futurewei.alcor.macmanager.service.MacService;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-public class MacControllerTest {
+public class MacControllerTest extends MockIgniteServer {
     private static final ObjectMapper om = new ObjectMapper();
-    public MacState testMacState;
-    String strTestMac = "";
 
     @Autowired
     MacService service;
@@ -56,48 +60,8 @@ public class MacControllerTest {
     @MockBean
     private MacController mockController;
 
-    @Before
-    public void init() {
-        MacState macState = new MacState("", "project1", "vpc1", "port1", "Active");
-        MacStateJson macStateJson = new MacStateJson(macState);
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        try {
-            String json = objectMapper.writeValueAsString(macStateJson);
-            MacState macState2 = service.createMacState(macState);
-            strTestMac = macState2.getMacAddress();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public String createMacState(MacState macState) {
-        MacStateJson macStateJson = new MacStateJson(macState);
-        ObjectMapper objectMapper = new ObjectMapper();
-        String strMacAddress = "";
-        try {
-            String json = objectMapper.writeValueAsString(macStateJson);
-            MacState macState2 = service.createMacState(macState);
-            strMacAddress = macState2.getMacAddress();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return strMacAddress;
-    }
-
-    public String createMacRange(MacRange macRange) {
-        MacRangeJson macRangeJson = new MacRangeJson(macRange);
-        ObjectMapper objectMapper = new ObjectMapper();
-        String strRangeId = "";
-
-        try {
-            String json = objectMapper.writeValueAsString(macRangeJson);
-            MacRange macRange2 = service.createMacRange(macRange);
-            strRangeId = macRange2.getRangeId();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return strRangeId;
+    @Test
+    void contextLoads() {
     }
 
     @Test
@@ -113,43 +77,69 @@ public class MacControllerTest {
         MacStateJson macStateJson = new MacStateJson(macState);
         ObjectMapper objectMapper = new ObjectMapper();
         String json = objectMapper.writeValueAsString(macStateJson);
-
         this.mockMvc.perform(post("/macs")
                 .content(json)
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated())
-                .andDo(print());
+                .andDo(print())
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    public void test_createMacState_invalidInput_null() throws Exception {
+        MacState macState = new MacState("", "project3", "vpc3", "port3", "Active");
+        MacStateJson macStateJson = null;
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(macStateJson);
+        this.mockMvc.perform(post("/macs")
+                .content(json)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
     }
 
     @Test
     public void test_getMacStateByMacAddress() throws Exception {
-        MacState macState = new MacState("", "project1", "vpc1", "port2", "Active");
-        String strTestMac = createMacState(macState);
+        MacState macState = new MacState("AA-BB-CC-01-01-01", "project1", "vpc1", "port2", "Active");
+        MacStateJson macStateJson = new MacStateJson(macState);
+        String strTestMac = "AA-BB-CC-01-01-01";
+        Mockito.when(mockController.getMacStateByMacAddress(strTestMac)).thenReturn(macStateJson);
         this.mockMvc.perform(get("/macs/" + strTestMac))
                 .andDo(print())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.mac_state.mac_address").value(strTestMac))
                 .andExpect(status().isOk());
     }
 
     @Test
+    public void test_getMacStateByMacAddress_invalidMac() throws Exception {
+        MacState macState = new MacState("AA-BB-CC-01-01-01", "project1", "vpc1", "port2", "Active");
+        MacStateJson macStateJson = new MacStateJson(macState);
+        String strTestMac = "AA-BB-CC-010101";
+        MvcResult result = this.mockMvc.perform(get("/macs/" + strTestMac))
+                .andDo(print())
+                .andReturn();
+        assertEquals(0, result.getResponse().getContentAsString().length());
+    }
+
+    @Test
     public void test_releaseMacStateByMacAddress() throws Exception {
-        MacState macState = new MacState("", "project1", "vpc1", "port3", "Active");
-        String strTestMac = createMacState(macState);
-        this.mockMvc.perform(delete("/macs/" + strTestMac))
+        MacState macState = new MacState("AA-BB-CC-01-01-03", "project1", "vpc1", "port3", "Active");
+        String strMacAddress = macState.getMacAddress();
+        Mockito.when(mockController.deleteMacAllocation(strMacAddress)).thenReturn(macState.getMacAddress());
+        this.mockMvc.perform(delete("/macs/" + strMacAddress))
                 .andDo(print())
                 .andExpect(status().isOk());
     }
 
     @Test
     public void test_activateMacState() throws Exception {
-        MacState macState = new MacState("", "project1", "vpc1", "port5", "Inactive");
-        String strTestMac = createMacState(macState);
-        MacStateJson macStateJson = new MacStateJson(macState);
-        System.out.println(macStateJson);
-        macState = new MacState(strTestMac, "project1", "vpc1", "port5", "Active");
-        macStateJson = new MacStateJson(macState);
+        MacState macState1 = new MacState("AA-BB-CC-01-01-05", "project1", "vpc1", "port5", "Inactive");
+        MacStateJson macStateJson1 = new MacStateJson(macState1);
+        MacState macState2 = new MacState("AA-BB-CC-01-01-05", "project1", "vpc1", "port5", "Active");
+        MacStateJson macStateJson2 = new MacStateJson(macState2);
+        String strTestMac = macState1.getMacAddress();
         ObjectMapper objectMapper = new ObjectMapper();
-        String json = objectMapper.writeValueAsString(macStateJson);
-
+        String json = objectMapper.writeValueAsString(macStateJson1);
+        Mockito.when(mockController.updateMacState(strTestMac, macStateJson1)).thenReturn(macStateJson2);
         this.mockMvc.perform(put("/macs/" + strTestMac)
                 .content(json)
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
@@ -159,15 +149,14 @@ public class MacControllerTest {
 
     @Test
     public void test_deactivateMacState() throws Exception {
-        MacState macState = new MacState("", "project1", "vpc1", "port7", "Active");
-        String strTestMac = createMacState(macState);
-        MacStateJson macStateJson = new MacStateJson(macState);
-        System.out.println(macStateJson);
-        macState = new MacState(strTestMac, "project1", "vpc1", "port7", "Inactive");
-        macStateJson = new MacStateJson(macState);
+        MacState macState1 = new MacState("AA-BB-CC-01-01-05", "project1", "vpc1", "port5", "Active");
+        MacStateJson macStateJson1 = new MacStateJson(macState1);
+        MacState macState2 = new MacState("AA-BB-CC-01-01-05", "project1", "vpc1", "port5", "Inactive");
+        MacStateJson macStateJson2 = new MacStateJson(macState2);
+        String strTestMac = macState1.getMacAddress();
         ObjectMapper objectMapper = new ObjectMapper();
-        String json = objectMapper.writeValueAsString(macStateJson);
-
+        String json = objectMapper.writeValueAsString(macStateJson1);
+        Mockito.when(mockController.updateMacState(strTestMac, macStateJson1)).thenReturn(macStateJson2);
         this.mockMvc.perform(put("/macs/" + strTestMac)
                 .content(json)
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
@@ -178,28 +167,35 @@ public class MacControllerTest {
     @Test
     public void test_getMacRangeByMacRangeId() throws Exception {
         MacRange macRange = new MacRange("range1", "00-AA-BB-11-11-11", "00-AA-BB-11-11-FF", "Active");
-        String strRangeId = createMacRange(macRange);
+        String strRangeId = macRange.getRangeId();
+        MacRangeJson macRangeJson = new MacRangeJson(macRange);
+        Mockito.when(mockController.getMacRangeByMacRangeId(strRangeId)).thenReturn(macRangeJson);
         this.mockMvc.perform(get("/macs/ranges/" + strRangeId))
                 .andDo(print())
                 .andExpect(status().isOk());
     }
 
-    public void test_getAllMacRanges(@PathVariable String rangeid) throws Exception {
-        MacRange macRange = new MacRange("range2", "00-AA-BB-11-22-22", "00-AA-BB-11-22-FF", "Active");
-        String strRangeId = createMacRange(macRange);
-        macRange = new MacRange("range3", "00-AA-BB-11-33-33", "00-AA-BB-11-FF", "Active");
-        strRangeId = createMacRange(macRange);
+    @Test
+    public void test_getAllMacRanges() throws Exception {
+        MacRange macRange2 = new MacRange("range2", "00-AA-BB-11-22-22", "00-AA-BB-11-22-FF", "Active");
+        MacRange macRange3 = new MacRange("range3", "00-AA-BB-11-33-33", "00-AA-BB-11-22-FF", "Active");
+        Collection<MacRange> ranges = new ArrayList<MacRange>();
+        ranges.add(macRange2);
+        ranges.add(macRange3);
+        Map<String, Collection<MacRange>> map = new HashMap<String, Collection<MacRange>>();
+        map.put("mac_ranges", ranges);
+        Mockito.when(mockController.getAllMacRanges()).thenReturn(map);
         this.mockMvc.perform(get("/macs/ranges"))
                 .andDo(print())
                 .andExpect(status().isOk());
     }
 
+    @Test
     public void test_createMacRange() throws Exception {
         MacRange macRange = new MacRange("range4", "00-AA-BB-11-00-11", "00-AA-BB-11-00-FF", "Active");
         MacRangeJson macRangeJson = new MacRangeJson(macRange);
         ObjectMapper objectMapper = new ObjectMapper();
         String json = objectMapper.writeValueAsString(macRangeJson);
-
         this.mockMvc.perform(post("/macs/ranges")
                 .content(json)
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
@@ -207,26 +203,40 @@ public class MacControllerTest {
                 .andDo(print());
     }
 
+    @Test
     public void updateMacRange() throws Exception {
         MacRange macRange = new MacRange("range5", "00-AA-BB-11-11-11", "00-AA-BB-55-55-55", "Inactive");
         MacRangeJson macRangeJson = new MacRangeJson(macRange);
+        MacRange macRange2 = new MacRange("range5", "00-AA-BB-11-11-11", "00-AA-BB-55-55-55", "Active");
+        MacRangeJson macRangeJson2 = new MacRangeJson(macRange2);
         ObjectMapper objectMapper = new ObjectMapper();
         String json = objectMapper.writeValueAsString(macRangeJson);
-
-        this.mockMvc.perform(put("/macs/ranges/range1")
+        Mockito.when(mockController.updateMacRange(macRange.getRangeId(), macRangeJson)).thenReturn(macRangeJson2);
+        this.mockMvc.perform(put("/macs/ranges/range5")
                 .content(json)
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated())
-                .andDo(print());
-    }
-
-    public void deleteMacRange() throws Exception {
-        MacRange macRange = new MacRange("range6", "00-AA-BB-11-22-22", "00-AA-BB-11-22-FF", "Active");
-        String strRangeId = createMacRange(macRange);
-        System.out.println(strRangeId);
-        this.mockMvc.perform(delete("/macs/ranges" + strTestMac))
                 .andDo(print())
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    public void deleteMacRange() throws Exception {
+        MacRange macRange = new MacRange("range6", "00-AA-BB-11-22-22", "00-AA-BB-11-22-FF", "Active");
+        String strRangeId = macRange.getRangeId();
+        ResponseId responseId = new ResponseId(strRangeId);
+        Mockito.when(mockController.deleteMacRange(strRangeId)).thenReturn(responseId);
+        this.mockMvc.perform(delete("/macs/ranges" + strRangeId))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void deleteMacRange_invalidId() throws Exception {
+        String strRangeId = "  ";
+        MvcResult result = this.mockMvc.perform(delete("/macs/ranges" + strRangeId))
+                .andDo(print())
+                .andReturn();
+        assertEquals(0, result.getResponse().getContentAsString().length());
     }
 }
 
