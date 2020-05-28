659919672a6b68f73cf2bbe2c0b8e47b51aa8c7d
Gzure <740684863@qq.com>
2020-05-28 16:12:00 +0800

    move neutron url prefix to application.properties

--- services/api_gateway/src/main/java/com/futurewei/alcor/apigateway/filter/KeystoneAuthWebFilter.java
+++ services/api_gateway/src/main/java/com/futurewei/alcor/apigateway/filter/KeystoneAuthWebFilter.java
@@ -2,6 +2,7 @@ package com.futurewei.alcor.apigateway.filter;
 
 import com.futurewei.alcor.apigateway.utils.KeystoneClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
@@ -10,8 +11,6 @@ import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
-
-import java.io.IOException;
 
 
 @Component
@@ -22,27 +21,25 @@ public class KeystoneAuthWebFilter implements WebFilter {
     @Autowired
     private KeystoneClient keystoneClient;
 
+    @Value("${neutron.url_prefix}")
+    private String neutronUrlPrefix;
+
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
         String token = exchange.getRequest().getHeaders().getFirst(AUTHORIZE_TOKEN);
-        try {
-            String projectId = keystoneClient.verifyToken(token);
-            if("".equals(projectId)){
-                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-                return exchange.getResponse().setComplete();
-            }
-            // rewrite uri path include project id
-            ServerHttpRequest req = exchange.getRequest();
-            ServerWebExchangeUtils.addOriginalRequestUrl(exchange, req.getURI());
-            String path = req.getURI().getRawPath();
-            String newPath = path.replaceAll("/v2.0", "/project/" + projectId);
-            ServerHttpRequest request = req.mutate().path(newPath).build();
-            exchange.getAttributes().put(ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR, request.getURI());
-            return chain.filter(exchange.mutate().request(request).build());
-        } catch (IOException e) {
-            exchange.getResponse().setStatusCode(HttpStatus.EXPECTATION_FAILED);
+        String projectId = keystoneClient.verifyToken(token);
+        if("".equals(projectId)){
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
             return exchange.getResponse().setComplete();
         }
+        // rewrite uri path include project id
+        ServerHttpRequest req = exchange.getRequest();
+        ServerWebExchangeUtils.addOriginalRequestUrl(exchange, req.getURI());
+        String path = req.getURI().getRawPath();
+        String newPath = path.replaceAll(neutronUrlPrefix, "/project/" + projectId);
+        ServerHttpRequest request = req.mutate().path(newPath).build();
+        exchange.getAttributes().put(ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR, request.getURI());
+        return chain.filter(exchange.mutate().request(request).build());
     }
 
 
