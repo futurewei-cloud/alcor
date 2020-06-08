3664db3fffa447a264f10cd4115fcd7302c0f673
albert <xzhang2@futurewei.com>
2020-06-08 00:42:56 -0700

    change entity for nb service

--- web/src/main/java/com/futurewei/alcor/web/entity/dataplane/InternalPortEntityNB.java
+++ web/src/main/java/com/futurewei/alcor/web/entity/dataplane/InternalPortEntityNB.java
@@ -2,19 +2,21 @@ package com.futurewei.alcor.web.entity.dataplane;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.futurewei.alcor.web.entity.port.PortEntity;
+import com.futurewei.alcor.web.entity.subnet.SubnetEntity;
+import com.futurewei.alcor.web.entity.vpc.VpcEntity;
+import lombok.Data;
 
-import java.util.List;
+import java.util.Set;
 
+@Data
 public class InternalPortEntityNB extends PortEntity {
 
-    @JsonProperty("neighbor_info")
-    private List<HostInfoNB> neighborInfo;
+  @JsonProperty("neighbor_host_ip")
+  private String neighborIp;
 
-    public List<HostInfoNB> getNeighborInfo() {
-        return neighborInfo;
-    }
+  @JsonProperty("binding_host_ip")
+  private String bindingHostIP;
 
-    public void setNeighborInfo(List<HostInfoNB> neighborInfo) {
-        this.neighborInfo = neighborInfo;
-    }
+  private Set<SubnetEntity> subnetEntities;
+  private Set<VpcEntity> vpcEntities;
 }
---
+++ web/src/main/java/com/futurewei/alcor/web/entity/dataplane/NetworkConfiguration.java
@@ -0,0 +1,28 @@
+package com.futurewei.alcor.web.entity.dataplane;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.futurewei.alcor.schema.Common;
+import com.futurewei.alcor.web.entity.securitygroup.SecurityGroupEntity;
+import com.futurewei.alcor.web.entity.subnet.SubnetEntity;
+import com.futurewei.alcor.web.entity.vpc.VpcEntity;
+import lombok.Data;
+
+@Data
+public class NetworkConfiguration {
+
+  private Common.ResourceType rsType;
+  private Common.OperationType opType;
+  boolean allOrNone = true;
+
+  @JsonProperty("ports")
+  private InternalPortEntityNB[] portStates;
+
+  @JsonProperty("vpcs")
+  private VpcEntity[] vpcs;
+
+  @JsonProperty("subnets")
+  private SubnetEntity[] subnets;
+
+  @JsonProperty("security_groups")
+  private SecurityGroupEntity[] securityGroupEntities;
+}
