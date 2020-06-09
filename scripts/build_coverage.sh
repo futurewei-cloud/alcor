a2b59680e2dd31aab6cc3df2170c09246ee57338
Eunju Kim <ekim@futurewei.com>
2020-06-08 23:42:10 -0700

    Add coverage script & yml files. Modiefied pom files for code coverage

--- scripts/build_coverage.sh
+++ scripts/build_coverage.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
 cd ./lib
-mvn package
+mvn test
 cd ..
 cd ./schema
-mvn package
+mvn test
 cd ..
 cd ./services/port_manager
-mvn install
+mvn test
 cd ../..
 cd ./services/private_ip_manager
-mvn install
+mvn test
 cd ../..
 cd ./services/subnet_manager
-mvn install
+mvn test
 cd ../..
 cd ./services/vpc_manager
-mvn install
+mvn test
 cd ../..
 cd ./services/route_manager
-mvn install
+mvn test
 cd ../..
 cd ./services/mac_manager
-mvn install
+mvn test
 cd ../..
 cd ./services/node_manager
-mvn install
+mvn test
