937e54347380c16f87d80a8fb4e3e33986358694
Wei Yuan <dakuaiera@sina.com>
2020-06-10 23:03:19 +0800

    upload elastic ip allocation initial codes

---
+++ lib/src/main/java/com/futurewei/alcor/common/db/DistributedLockFactory.java
@@ -0,0 +1,61 @@
+/*
+Copyright 2019 The Alcor Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.
+*/
+package com.futurewei.alcor.common.db;
+
+import com.futurewei.alcor.common.db.ignite.IgniteDistributedLock;
+import com.futurewei.alcor.common.db.redis.RedisDistributedLock;
+import org.apache.ignite.client.IgniteClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+@ComponentScan
+@Component
+public class DistributedLockFactory {
+    @Autowired(required = false)
+    private IgniteClient igniteClient;
+
+    @Value("${lock.try.interval}")
+    private int tryLockInterval;
+
+    @Value("${lock.expire.time}")
+    private int expireTime;
+
+    @Autowired
+    LettuceConnectionFactory lettuceConnectionFactory;
+
+    private IDistributedLock getIgniteDistributedLock(String name) {
+        return new IgniteDistributedLock(igniteClient, name, tryLockInterval, expireTime);
+    }
+
+    public IDistributedLock getRedisDistributedLock(String name) {
+        StringRedisTemplate template = new StringRedisTemplate();
+        template.setConnectionFactory(lettuceConnectionFactory);
+
+        return new RedisDistributedLock(template, name, tryLockInterval, expireTime);
+    }
+
+    public <T> IDistributedLock getDistributedLock(Class<T> t) {
+        if (igniteClient != null) {
+            return getIgniteDistributedLock(t.getName());
+        }
+
+        return getRedisDistributedLock(t.getName());
+    }
+}
---
+++ services/elastic_ip_manager/src/main/java/com/futurewei/alcor/elasticipmanager/dao/RepoResource.java
@@ -0,0 +1,68 @@
+package com.futurewei.alcor.elasticipmanager.dao;
+
+import com.futurewei.alcor.common.db.CacheException;
+import com.futurewei.alcor.common.db.CacheFactory;
+import com.futurewei.alcor.common.db.ICache;
+import com.futurewei.alcor.common.db.repo.ICacheRepository;
+import com.futurewei.alcor.common.entity.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public abstract class RepoResource<T extends Resource> implements ICacheRepository<T> {
+    private static final Logger LOG = LoggerFactory.getLogger(RepoResource.class);
+    private ICache<String, T> cache;
+
+    public abstract Class<T> getResourceClass();
+
+    public RepoResource(CacheFactory cacheFactory) {
+        cache = cacheFactory.getCache(getResourceClass());
+    }
+
+    @Override
+    public T findItem(String id) {
+        try {
+            return cache.get(id);
+        } catch (CacheException e) {
+            e.printStackTrace();
+            LOG.error(this.getClass().getName() + " findItem() exception:", e);
+        }
+
+        return null;
+    }
+
+    @Override
+    public Map<String, T> findAllItems() {
+        try {
+            return cache.getAll();
+        } catch (CacheException e) {
+            e.printStackTrace();
+            LOG.error(this.getClass().getName() + " findAllItems() exception:", e);
+        }
+
+        return new HashMap<String, T>();
+    }
+
+    @Override
+    public void addItem(T resource) {
+        try {
+            cache.put(resource.getId(), resource);
+        } catch (CacheException e) {
+            e.printStackTrace();
+            LOG.error(this.getClass().getName() + " addItem() exception:", e);
+        }
+    }
+
+    @Override
+    public void deleteItem(String resourceId) {
+        try {
+            cache.remove(resourceId);
+        } catch (CacheException e) {
+            e.printStackTrace();
+            LOG.error(this.getClass().getName() + " deleteItem() exception:", e);
+        }
+    }
+}
