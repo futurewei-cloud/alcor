ede96f22b3a14cc3583d516212fc81632dd6d3b4
Eunju Kim <ekim@futurewei.com>
2020-06-10 10:27:04 -0700

    [Code Coverage] - modified build_coverage.sh

--- scripts/build_coverage.sh
+++ scripts/build_coverage.sh
@@ -6,29 +6,14 @@ cd ..
 cd ./schema
 mvn compile
 cd ..
-cd ./services/port_manager
-mvn test
-cd ../..
-cd ./services/private_ip_manager
-mvn test
-cd ../..
-cd ./services/subnet_manager
-mvn test
-cd ../..
-cd ./services/vpc_manager
-mvn test
-cd ../..
-cd ./services/route_manager
-mvn test
-cd ../..
-cd ./services/mac_manager
-mvn test
-cd ../..
-cd ./services/node_manager
-mvn test
-cd ../..
-cd ./services/api_gateway
-mvn test
-cd ../..
 cd ./web
 mvn compile
+
+echo "Build service one by one under services directory"
+cd services
+for d in ./*/;
+do
+    cd $d
+    mvn test
+    cd ..
+done
