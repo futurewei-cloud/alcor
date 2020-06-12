54f5cf26d33f5dae81df0d44c3eea87396c80911
Liguang Xie <lxie@futurewei.com>
2020-06-11 21:57:10 -0700

    Minor fix format

--- services/security_group_manager/src/main/java/com/futurewei/alcor/securitygroup/config/SwaggerConfig.java
+++ services/security_group_manager/src/main/java/com/futurewei/alcor/securitygroup/config/SwaggerConfig.java
@@ -30,7 +30,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
     @Bean
-    public Docket api(){
+    public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
@@ -40,7 +40,7 @@ public class SwaggerConfig {
                 .build();
     }
 
-    private ApiInfo apiInfo(){
+    private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("Security Group Manager")
                 .description("Security Group lifecycle management")
