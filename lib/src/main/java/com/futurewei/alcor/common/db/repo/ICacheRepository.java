9b5abb9c37715e133129adb53f4b1743c6ccc5f5
Kevin <zhonghaolyu@gmail.com>
2020-07-02 16:22:37 -0700

    update

--- lib/src/main/java/com/futurewei/alcor/common/db/repo/ICacheRepository.java
+++ lib/src/main/java/com/futurewei/alcor/common/db/repo/ICacheRepository.java
@@ -27,9 +27,9 @@ public interface ICacheRepository<T> {
 
     Map<String, T> findAllItems() throws CacheException;
 
-    void addItem(T newItem) throws Exception;
+    void addItem(T newItem) throws CacheException;
 
     //void addItems(List<T> items) throws CacheException;
 
-    void deleteItem(String id) throws Exception;
+    void deleteItem(String id) throws CacheException;
 }
