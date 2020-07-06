d572a6516700e3a0fa343802f044fb606c26081e
Wei Yuan <dakuaiera@sina.com>
2020-07-06 23:07:50 +0800

    update codes

--- services/elastic_ip_manager/src/main/java/com/futurewei/alcor/elasticipmanager/controller/ElasticIpController.java
+++ services/elastic_ip_manager/src/main/java/com/futurewei/alcor/elasticipmanager/controller/ElasticIpController.java
@@ -17,19 +17,11 @@ Licensed under the Apache License, Version 2.0 (the "License");
 package com.futurewei.alcor.elasticipmanager.controller;
 
 import com.futurewei.alcor.common.entity.ResponseId;
-import com.futurewei.alcor.common.utils.Ipv4AddrUtil;
-import com.futurewei.alcor.common.utils.Ipv6AddrUtil;
-import com.futurewei.alcor.elasticipmanager.config.IpVersion;
-import com.futurewei.alcor.elasticipmanager.exception.ElasticIpIdConfilictException;
 import com.futurewei.alcor.elasticipmanager.exception.ElasticIpNoProjectIdException;
-import com.futurewei.alcor.elasticipmanager.exception.ElasticIpProjectIdConflictException;
 import com.futurewei.alcor.elasticipmanager.exception.elasticip.*;
-import com.futurewei.alcor.elasticipmanager.exception.ElasticIpQueryFormatException;
-import com.futurewei.alcor.elasticipmanager.exception.elasticiprange.ElasticIpRangeBadRangesException;
-import com.futurewei.alcor.elasticipmanager.exception.elasticiprange.ElasticIpRangeNoIdException;
-import com.futurewei.alcor.elasticipmanager.exception.elasticiprange.ElasticIpRangeVersionException;
 import com.futurewei.alcor.elasticipmanager.service.ElasticIpRangeService;
 import com.futurewei.alcor.elasticipmanager.service.ElasticIpService;
+import com.futurewei.alcor.elasticipmanager.utils.ElasticIpControllerUtils;
 import com.futurewei.alcor.web.entity.elasticip.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -38,8 +30,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
-import java.math.BigInteger;
-import java.util.ArrayList;
 import java.util.List;
 
 
@@ -52,236 +42,6 @@ public class ElasticIpController {
 
     @Autowired
     ElasticIpRangeService elasticIpRangeService;
-
-    private boolean isIpAddressInvalid(String ipAddress, Integer ipVersion) {
-        boolean isInvalid = true;
-        if (ipVersion.equals(IpVersion.IPV4.getVersion())) {
-            isInvalid = !Ipv4AddrUtil.formatCheck(ipAddress);
-        } else if (ipVersion.equals(IpVersion.IPV6.getVersion())) {
-            isInvalid = !Ipv6AddrUtil.formatCheck(ipAddress);
-        }
-
-        return isInvalid;
-    }
-
-    private boolean isIpVersionInvalid(Integer ipVersion) {
-        return !ipVersion.equals(IpVersion.IPV4.getVersion()) && !ipVersion.equals(IpVersion.IPV6.getVersion());
-    }
-
-    private boolean isAllocationRangesInvalid(Integer ipVersion,
-                                              List<ElasticIpRange.AllocationRange> allocationRanges) {
-
-        boolean isInvalid = false;
-        if (allocationRanges != null) {
-
-            try {
-                if (ipVersion.equals(IpVersion.IPV4.getVersion())) {
-                    for (ElasticIpRange.AllocationRange range: allocationRanges) {
-                        long start = Ipv4AddrUtil.ipv4ToLong(range.getStart());
-                        long end = Ipv4AddrUtil.ipv4ToLong(range.getEnd());
-                        if (start > end) {
-                            isInvalid = true;
-                        }
-                    }
-                } else if (ipVersion.equals(IpVersion.IPV6.getVersion())) {
-                    for (ElasticIpRange.AllocationRange range: allocationRanges) {
-                        BigInteger start = Ipv6AddrUtil.ipv6ToBitInt(range.getStart());
-                        BigInteger end = Ipv6AddrUtil.ipv6ToBitInt(range.getEnd());
-                        if (start.compareTo(end) > 0) {
-                            isInvalid = true;
-                        }
-                    }
-                }
-            } catch (NumberFormatException e) {
-                isInvalid = true;
-            }
-        }
-        return isInvalid;
-    }
-
-    private void createElasticIpParameterProcess(String projectId, ElasticIpInfo elasticIpInfo) throws Exception {
-
-        if (elasticIpInfo == null) {
-            throw new ElasticIpQueryFormatException();
-        }
-
-        if (StringUtils.isEmpty(projectId)) {
-            throw new ElasticIpNoProjectIdException();
-        } else if (elasticIpInfo.getProjectId() == null) {
-            elasticIpInfo.setProjectId(projectId);
-        } else if (!projectId.equals(elasticIpInfo.getProjectId())) {
-            throw new ElasticIpProjectIdConflictException();
-        }
-
-        if (elasticIpInfo.getElasticIpVersion() == null) {
-            elasticIpInfo.setElasticIpVersion(IpVersion.IPV4.getVersion());
-        } else if (this.isIpVersionInvalid(elasticIpInfo.getElasticIpVersion())) {
-            throw new ElasticIpEipVersionException();
-        }
-
-        if (elasticIpInfo.getElasticIp() != null) {
-            if (this.isIpAddressInvalid(elasticIpInfo.getElasticIp(), elasticIpInfo.getElasticIpVersion())) {
-                throw new ElasticIpEipAddressException();
-            }
-        }
-
-        if (elasticIpInfo.getPortId() == null) {
-            elasticIpInfo.setPortId("");
-        }
-        if (elasticIpInfo.getPortId().isEmpty()) {
-            if (elasticIpInfo.getPrivateIp()!= null || elasticIpInfo.getPrivateIpVersion() != null) {
-                throw new ElasticIpNoPortIdException();
-            }
-        } else {
-            if (elasticIpInfo.getPrivateIpVersion() != null &&
-                    this.isIpVersionInvalid(elasticIpInfo.getPrivateIpVersion())) {
-                throw new ElasticIpPipVersionException();
-            }
-
-            if (elasticIpInfo.getPrivateIp() != null) {
-                if (elasticIpInfo.getPrivateIpVersion() == null) {
-                    elasticIpInfo.setPrivateIpVersion(IpVersion.IPV4.getVersion());
-                }
-                if (this.isIpAddressInvalid(elasticIpInfo.getPrivateIp(), elasticIpInfo.getPrivateIpVersion())) {
-                    throw new ElasticIpPipAddressException();
-                }
-            }
-        }
-
-        if (elasticIpInfo.getName() == null) {
-            elasticIpInfo.setName("");
-        }
-
-        if (elasticIpInfo.getDescription() == null) {
-            elasticIpInfo.setDescription("");
-        }
-
-        if (elasticIpInfo.getDnsDomain() == null) {
-            elasticIpInfo.setDnsDomain("");
-        }
-
-        if (elasticIpInfo.getDnsName() == null) {
-            elasticIpInfo.setDnsName("");
-        }
-    }
-
-    private void updateElasticIpParameterProcess(String projectId, String elasticIpId, ElasticIpInfo elasticIpInfo)
-            throws Exception {
-
-        if (elasticIpInfo == null) {
-            throw new ElasticIpQueryFormatException();
-        }
-
-        if (StringUtils.isEmpty(projectId)) {
-            throw new ElasticIpNoProjectIdException();
-        } else if (elasticIpInfo.getProjectId() == null) {
-            elasticIpInfo.setProjectId(projectId);
-        } else if (!projectId.equals(elasticIpInfo.getProjectId())) {
-            throw new ElasticIpProjectIdConflictException();
-        }
-
-        if (StringUtils.isEmpty(elasticIpId)) {
-            throw new ElasticIpNoIdException();
-        } else if (elasticIpInfo.getId() == null) {
-            elasticIpInfo.setId(elasticIpId);
-        } else if (!elasticIpId.equals(elasticIpInfo.getId())) {
-            throw new ElasticIpIdConfilictException();
-        }
-
-        if (elasticIpInfo.getElasticIpVersion() != null &&
-                this.isIpVersionInvalid(elasticIpInfo.getElasticIpVersion())) {
-            throw new ElasticIpEipVersionException();
-        }
-
-        if (elasticIpInfo.getElasticIp() != null) {
-            if (elasticIpInfo.getElasticIpVersion() == null) {
-                elasticIpInfo.setElasticIpVersion(IpVersion.IPV4.getVersion());
-            }
-            if (this.isIpAddressInvalid(elasticIpInfo.getElasticIp(), elasticIpInfo.getElasticIpVersion())) {
-                throw new ElasticIpEipAddressException();
-            }
-        }
-
-        if (StringUtils.isEmpty(elasticIpInfo.getPortId())) {
-            if (elasticIpInfo.getPrivateIp() != null || elasticIpInfo.getPrivateIpVersion() != null) {
-                throw new ElasticIpNoPortIdException();
-            }
-        } else {
-            if (elasticIpInfo.getPrivateIpVersion() != null &&
-                    this.isIpVersionInvalid(elasticIpInfo.getPrivateIpVersion())) {
-                throw new ElasticIpPipVersionException();
-            }
-
-            if (elasticIpInfo.getPrivateIp() != null) {
-                if (elasticIpInfo.getPrivateIp() == null) {
-                    elasticIpInfo.setPrivateIpVersion(IpVersion.IPV4.getVersion());
-                }
-                if (this.isIpAddressInvalid(elasticIpInfo.getPrivateIp(), elasticIpInfo.getPrivateIpVersion())) {
-                    throw new ElasticIpPipAddressException();
-                }
-            }
-        }
-    }
-
-    private void createElasticIpRangeParameterProcess(ElasticIpRangeInfo elasticIpRangeInfo) throws Exception {
-
-        if (elasticIpRangeInfo == null) {
-            throw new ElasticIpQueryFormatException();
-        }
-
-        if (elasticIpRangeInfo.getIpVersion() == null) {
-            elasticIpRangeInfo.setIpVersion(IpVersion.IPV4.getVersion());
-        } else if (this.isIpVersionInvalid(elasticIpRangeInfo.getIpVersion())) {
-            throw new ElasticIpRangeVersionException();
-        }
-
-        List<ElasticIpRange.AllocationRange> allocationRanges = elasticIpRangeInfo.getAllocationRanges();
-        if (allocationRanges != null) {
-            if (this.isAllocationRangesInvalid(elasticIpRangeInfo.getIpVersion(), allocationRanges)) {
-                throw new ElasticIpRangeBadRangesException();
-            }
-        } else {
-            elasticIpRangeInfo.setAllocationRanges(new ArrayList<>());
-        }
-
-        if (elasticIpRangeInfo.getName() == null) {
-            elasticIpRangeInfo.setName("");
-        }
-
-        if (elasticIpRangeInfo.getDescription() == null) {
-            elasticIpRangeInfo.setDescription("");
-        }
-    }
-
-    private void updateElasticIpRangeParameterProcess(String elasticipRangeId,
-                                                      ElasticIpRangeInfo elasticIpRangeInfo) throws Exception {
-
-        if (elasticIpRangeInfo == null) {
-            throw new ElasticIpQueryFormatException();
-        }
-
-        if (StringUtils.isEmpty(elasticipRangeId)) {
-            throw new ElasticIpRangeNoIdException();
-        } else if (elasticIpRangeInfo.getId() == null) {
-            elasticIpRangeInfo.setId(elasticipRangeId);
-        } else if (!elasticipRangeId.equals(elasticIpRangeInfo.getId())) {
-            throw new ElasticIpIdConfilictException();
-        }
-
-        if (elasticIpRangeInfo.getIpVersion() != null && this.isIpVersionInvalid(elasticIpRangeInfo.getIpVersion())) {
-            throw new ElasticIpRangeVersionException();
-        }
-
-        List<ElasticIpRange.AllocationRange> allocationRanges = elasticIpRangeInfo.getAllocationRanges();
-        if (allocationRanges != null) {
-            if (elasticIpRangeInfo.getIpVersion() == null) {
-                elasticIpRangeInfo.setIpVersion(IpVersion.IPV4.getVersion());
-            }
-            if (this.isAllocationRangesInvalid(elasticIpRangeInfo.getIpVersion(), allocationRanges)) {
-                throw new ElasticIpRangeBadRangesException();
-            }
-        }
-    }
 
     /**
      * Create an elastic ip, and communicate with port and node services if the
@@ -299,7 +59,7 @@ public class ElasticIpController {
 
         ElasticIpInfo requestInfo = request.getElasticip();
 
-        this.createElasticIpParameterProcess(projectId, requestInfo);
+        ElasticIpControllerUtils.createElasticIpParameterProcess(projectId, requestInfo);
 
         ElasticIpInfo result = elasticipService.createElasticIp(requestInfo);
 
@@ -323,7 +83,7 @@ public class ElasticIpController {
 
         ElasticIpInfo requestInfo = request.getElasticip();
 
-        this.updateElasticIpParameterProcess(projectId, elasticIpId, requestInfo);
+        ElasticIpControllerUtils.updateElasticIpParameterProcess(projectId, elasticIpId, requestInfo);
 
         ElasticIpInfo result = elasticipService.updateElasticIp(requestInfo);
 
@@ -414,7 +174,7 @@ public class ElasticIpController {
 
         ElasticIpRangeInfo requestInfo = request.getElasticIpRange();
 
-        this.createElasticIpRangeParameterProcess(requestInfo);
+        ElasticIpControllerUtils.createElasticIpRangeParameterProcess(requestInfo);
 
         ElasticIpRangeInfo result = elasticIpRangeService.createElasticIpRange(requestInfo);
 
@@ -436,7 +196,7 @@ public class ElasticIpController {
 
         ElasticIpRangeInfo requestInfo = request.getElasticIpRange();
 
-        this.updateElasticIpRangeParameterProcess(elasticIpRangeId, requestInfo);
+        ElasticIpControllerUtils.updateElasticIpRangeParameterProcess(elasticIpRangeId, requestInfo);
 
         ElasticIpRangeInfo result = elasticIpRangeService.updateElasticIpRange(requestInfo);
 
