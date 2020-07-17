bac6728eac9d11916fef5ca2e098c6af9a9a0fa7
Kevin <zhonghaolyu@gmail.com>
2020-07-16 22:04:18 -0700

    update

--- services/route_manager/src/main/java/com/futurewei/alcor/route/controller/RouteController.java
+++ services/route_manager/src/main/java/com/futurewei/alcor/route/controller/RouteController.java
@@ -36,7 +36,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
