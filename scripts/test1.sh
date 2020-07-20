410435514cc9da4826fead98dd1fa76b8fd838b5
albert <xzhang2@futurewei.com>
2020-07-20 15:04:14 -0700

    reorg

--- scripts/test1.sh
+++ scripts/test1.sh
@@ -1,5 +1,5 @@
-curl -X POST -H "Content-Type: application/json" --data @cvpc.json http://localhost:9009/project/3dda2801-d675-4688-a63f-dcda8d327f50/vpcs
-curl -X POST -H "Content-Type: application/json" --data @csn.json http://localhost:9009/project/3dda2801-d675-4688-a63f-dcda8d327f50/subnets
-curl -X POST -H "Content-Type: application/json" --data @node.json http://localhost:9007/v4/nodes
-curl -X POST -H "Content-Type: application/json" --data @sg.json http://localhost:9008/v4/3dda2801-d675-4688-a63f-dcda8d327f50/security-groups
-curl -X POST -H "Content-Type: application/json" --data @cp1b.json http://localhost:9009/project/3dda2801-d675-4688-a63f-dcda8d327f50/ports
+curl -X POST -H "Content-Type: application/json" --data @json/cvpc.json http://localhost:9009/project/3dda2801-d675-4688-a63f-dcda8d327f50/vpcs
+curl -X POST -H "Content-Type: application/json" --data @json/csn.json http://localhost:9009/project/3dda2801-d675-4688-a63f-dcda8d327f50/subnets
+curl -X POST -H "Content-Type: application/json" --data @json/node.json http://localhost:9007/v4/nodes
+curl -X POST -H "Content-Type: application/json" --data @json/sg.json http://localhost:9008/v4/3dda2801-d675-4688-a63f-dcda8d327f50/security-groups
+curl -X POST -H "Content-Type: application/json" --data @json/cp1b.json http://localhost:9009/project/3dda2801-d675-4688-a63f-dcda8d327f50/ports
