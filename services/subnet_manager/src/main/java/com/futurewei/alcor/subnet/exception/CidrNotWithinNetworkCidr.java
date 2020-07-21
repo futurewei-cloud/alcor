59c090beca78fc089cd3e156fc46b290559d7347
Wei Yuan <dakuaiera@sina.com>
2020-07-21 16:21:29 +0800

    Fixed some issues according to the review comments 1) use a support custom defined message exception instead of the former fixed message exception 2) cancel the limitation of specifying an ipv6 private ip 3) cancel the limitation of associating a multi fixed ips port and not specifying the private ip to be associated 4) fixed a bug that allocated ip would not be released when a exception occurs at associating port process

--- services/subnet_manager/src/main/java/com/futurewei/alcor/subnet/exception/CidrNotWithinNetworkCidr.java
+++ services/subnet_manager/src/main/java/com/futurewei/alcor/subnet/exception/CidrNotWithinNetworkCidr.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:services/subnet_manager/src/main/java/com/futurewei/alcor/subnet/exception/CidrNotWithinNetworkCidr.java
 /*
 Copyright 2019 The Alcor Authors.
 
@@ -21,3 +22,38 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(code= HttpStatus.INTERNAL_SERVER_ERROR, reason="cidr is not within network cidr")
 public class CidrNotWithinNetworkCidr extends Exception{
 }
+=======
+/*
+Copyright 2020 The Alcor Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.
+*/
+
+package com.futurewei.alcor.elasticipmanager.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class ElasticIpCommonException extends Exception {
+
+    private final HttpStatus httpStatus;
+
+    public ElasticIpCommonException(HttpStatus httpStatus, String msg) {
+        super(msg);
+        this.httpStatus = httpStatus;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return this.httpStatus;
+    }
+}
+>>>>>>> a18c3a7... Fixed some issues according to the review comments:services/elastic_ip_manager/src/main/java/com/futurewei/alcor/elasticipmanager/exception/ElasticIpCommonException.java
