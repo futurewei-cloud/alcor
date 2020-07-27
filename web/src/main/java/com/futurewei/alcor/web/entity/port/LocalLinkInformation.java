aca63deefcfe1b7fd961d506d0e337f6fbbe3d01
albert <xzhang2@futurewei.com>
2020-07-27 12:19:13 -0700

    remove default value for LocalLinkInformation

--- web/src/main/java/com/futurewei/alcor/web/entity/port/LocalLinkInformation.java
+++ web/src/main/java/com/futurewei/alcor/web/entity/port/LocalLinkInformation.java
@@ -4,9 +4,33 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class LocalLinkInformation {
     @JsonProperty("port_id")
-    private  String port_id="Ethernet3/1";
+    private  String port_id;
     @JsonProperty("switch_id")
-    private String switch_id="0a:1b:2c:3d:4e:5f";
+    private String switch_id;
     @JsonProperty("switch_info")
-    private String switch_info="switch1";
+    private String switch_info;
+
+    public String getPort_id() {
+        return port_id;
+    }
+
+    public void setPort_id(String port_id) {
+        this.port_id = port_id;
+    }
+
+    public String getSwitch_id() {
+        return switch_id;
+    }
+
+    public void setSwitch_id(String switch_id) {
+        this.switch_id = switch_id;
+    }
+
+    public String getSwitch_info() {
+        return switch_info;
+    }
+
+    public void setSwitch_info(String switch_info) {
+        this.switch_info = switch_info;
+    }
 }
