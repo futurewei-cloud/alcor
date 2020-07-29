8b1cb0642a03d1e9c43845153bc382f43fdfc29e
Gzure <740684863@qq.com>
2020-07-29 15:32:36 +0800

    remove exception import

--- services/api_gateway/src/main/java/com/futurewei/alcor/apigateway/client/KeystoneClient.java
+++ services/api_gateway/src/main/java/com/futurewei/alcor/apigateway/client/KeystoneClient.java
@@ -21,7 +21,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.futurewei.alcor.apigateway.exception.KeystoneVersionNotSupportException;
 import com.futurewei.alcor.common.db.CacheException;
 import com.futurewei.alcor.common.db.CacheFactory;
 import com.futurewei.alcor.common.db.ICache;
