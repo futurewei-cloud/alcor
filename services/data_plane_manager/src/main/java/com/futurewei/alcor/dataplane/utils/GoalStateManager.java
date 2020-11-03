deb44dc701592f4c8bd8bca2ac8e55fd6f2dfbea
Kevin <zhonghaolyu@gmail.com>
2020-11-03 10:02:34 -0800

    update

--- services/data_plane_manager/src/main/java/com/futurewei/alcor/dataplane/utils/GoalStateManager.java
+++ services/data_plane_manager/src/main/java/com/futurewei/alcor/dataplane/utils/GoalStateManager.java
@@ -6,7 +6,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
         You may obtain a copy of the License at
 
         http://www.apache.org/licenses/LICENSE-2.0
-        
+
         Unless required by applicable law or agreed to in writing, software
         distributed under the License is distributed on an "AS IS" BASIS,
         WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -471,6 +471,7 @@ public class GoalStateManager {
         tempPorts.add(neighborInfo.getPortId());
       }
     }
+
      */
 
         portsInSameSubnetMap.put(fixedIp.getSubnetId(), tempPorts);
