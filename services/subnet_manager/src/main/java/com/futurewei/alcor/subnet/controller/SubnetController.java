28af26b782df496b123768cde56bff15159e366d
Kevin <zhonghaolyu@gmail.com>
2020-11-04 11:10:03 -0800

    update

--- services/subnet_manager/src/main/java/com/futurewei/alcor/subnet/controller/SubnetController.java
+++ services/subnet_manager/src/main/java/com/futurewei/alcor/subnet/controller/SubnetController.java
@@ -474,13 +474,9 @@ public class SubnetController {
             // delete subnet id in vpc
             this.subnetService.deleteSubnetIdInVpc(subnetId, projectId, subnetEntity.getVpcId());
 
-        } catch (ParameterNullOrEmptyException e) {
+        } catch (ParameterNullOrEmptyException | HavePortInSubnet | SubnetBindRoutes e) {
             logger.error(e.getMessage());
             throw new Exception(e);
-        } catch (HavePortInSubnet e) {
-            throw e;
-        } catch (SubnetBindRoutes e) {
-            throw e;
         }
 
         return new ResponseId(subnetId);
