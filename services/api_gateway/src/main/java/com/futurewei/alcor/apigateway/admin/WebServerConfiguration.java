13007739c562167a1febb4177d90d975fec77a13
Liguang Xie <lxie@futurewei.com>
2020-11-05 22:19:23 -0800

    Correct typo in the comment

--- services/api_gateway/src/main/java/com/futurewei/alcor/apigateway/admin/WebServerConfiguration.java
+++ services/api_gateway/src/main/java/com/futurewei/alcor/apigateway/admin/WebServerConfiguration.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class WebServerConfiguration implements WebServerFactoryCustomizer<NettyReactiveWebServerFactory> {
 
-    // The default value is 8192 (8K) but may result in 413 when header is too lager.
+    // The default value is 8192 (8K) but may result in 413 when header is too large.
     // Enlarge the header size to 65536 (64K) which is enough for most cases.
     @Value("${server.max-header-size:65536}")
     private int maxHeaderSize;
