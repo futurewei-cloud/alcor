45a9025cb546b573d49aab150c13988c1fad00ee
Xiaodong Zhang <xiaodongzhang@Xiaodongs-MacBook-Pro.local>
2020-12-16 14:18:10 -0800

    add null check

--- lib/src/main/java/com/futurewei/alcor/common/config/JaegerTracerHelper.java
+++ lib/src/main/java/com/futurewei/alcor/common/config/JaegerTracerHelper.java
@@ -3,12 +3,16 @@ package com.futurewei.alcor.common.config;
 import io.jaegertracing.Configuration;
 import io.jaegertracing.internal.JaegerTracer;
 import io.jaegertracing.internal.samplers.ConstSampler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class JaegerTracerHelper {
+    @Autowired
+    private JaegerConfig jaegerConfig;
 
     public JaegerTracer initTracer(String service) {
 
-        JaegerConfig jaegerConfig = new JaegerConfig();
         Configuration.SamplerConfiguration samplerConfig = new Configuration.SamplerConfiguration()
                 .withType(ConstSampler.TYPE)
                 .withParam(1);
