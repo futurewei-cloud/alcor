10d534de20b71b463e1ff4904caf4ddc978723fb
Zhonghao(Kevin) Lyu <52331056+kevin-zhonghao@users.noreply.github.com>
2020-12-17 18:58:00 -0800

    [Bug Fix] Fix Issue #455 and Issue #274 (#514)

--- services/route_manager/src/main/java/com/futurewei/alcor/route/dao/RouteTableRepository.java
+++ services/route_manager/src/main/java/com/futurewei/alcor/route/dao/RouteTableRepository.java
@@ -18,6 +18,7 @@ package com.futurewei.alcor.route.dao;
 import com.futurewei.alcor.common.db.CacheException;
 import com.futurewei.alcor.common.db.CacheFactory;
 import com.futurewei.alcor.common.db.ICache;
+import com.futurewei.alcor.common.db.Transaction;
 import com.futurewei.alcor.common.db.repo.ICacheRepository;
 import com.futurewei.alcor.common.logging.Logger;
 import com.futurewei.alcor.common.logging.LoggerFactory;
@@ -71,15 +72,28 @@ public class RouteTableRepository implements ICacheRepository<RouteTable> {
     @Override
     @DurationStatistics
     public void addItem(RouteTable routeTable) throws CacheException {
-        logger.log(Level.INFO, "Add route table, route table Id:" + routeTable.getId());
-        cache.put(routeTable.getId(), routeTable);
+        try (Transaction tx = cache.getTransaction().start()) {
 
+            logger.log(Level.INFO, "Add route table, route table Id:" + routeTable.getId());
+            cache.put(routeTable.getId(), routeTable);
+
+            tx.commit();
+        } catch (Exception e) {
+            throw new CacheException();
+        }
     }
 
     @Override
     @DurationStatistics
     public void deleteItem(String id) throws CacheException {
-        logger.log(Level.INFO, "Delete route table, route table Id:" + id);
-        cache.remove(id);
+        try (Transaction tx = cache.getTransaction().start()) {
+
+            logger.log(Level.INFO, "Delete route table, route table Id:" + id);
+            cache.remove(id);
+
+            tx.commit();
+        } catch (Exception e) {
+            throw new CacheException();
+        }
     }
 }
