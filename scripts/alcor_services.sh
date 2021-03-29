068262b80e0f4cd616d6272565fbadd80070ec76
Kiran Rao <ktangeed@futurewei.com>
2021-03-29 11:55:43 -0400

    Ping test python automation added to scripts folder

--- scripts/alcor_services.sh
+++ scripts/alcor_services.sh
@@ -9,36 +9,70 @@ function build_alcor_images()
     cd $ALCOR_ROOT_DIR
     mvn -Dmaven.test.skip=true -DskipTests clean package install
     #build images
-    echo "Creating ignite-11 image with ports 10800 10081 47100 47500"
+    echo "#0 Creating ignite-11 image with ports 10800 10081 47100 47500"
     docker build -t ignite-11 -f $ALCOR_ROOT_DIR/lib/ignite.Dockerfile $ALCOR_ROOT_DIR/lib
-    echo "Creating vpc_manager image with port 9001"
+    echo
+
+    echo "#1 Creating vpc_manager image with port 9001"
     docker build -t vpm $ALCOR_ROOT_DIR/services/vpc_manager/
-    echo "Creating subnet_manager image with port 9002"
+    echo
+
+    echo "#2 Creating subnet_manager image with port 9002"
     docker build -t snm $ALCOR_ROOT_DIR/services/subnet_manager/
-    echo "Creating route_manager image with port 9003"
+    echo
+
+    echo "#3 Creating route_manager image with port 9003"
     docker build -t rm $ALCOR_ROOT_DIR/services/route_manager/
-    echo "Creating private_ip_manager image with port 9004"
+    echo
+
+    echo "#4 Creating private_ip_manager image with port 9004"
     docker build -t pim $ALCOR_ROOT_DIR/services/private_ip_manager/
-    echo "Creating mac_manager image with port 9005"
+    echo
+
+    echo "#5 Creating mac_manager image with port 9005"
     docker build -t mm $ALCOR_ROOT_DIR/services/mac_manager/
-    echo "Creating port_manger image with port 9006"
+    echo
+
+    echo "#6 Creating port_manger image with port 9006"
     docker build -t pm $ALCOR_ROOT_DIR/services/port_manager/
-    echo "Creating node_manager image with port 9007"
+    echo
+
+    echo "#7 Creating node_manager image with port 9007"
     docker build -t nm $ALCOR_ROOT_DIR/services/node_manager/
-    echo "Creating security_group_manager image with port 9008"
+    echo
+
+    echo "#8 Creating security_group_manager image with port 9008"
     docker build -t sgm $ALCOR_ROOT_DIR/services/security_group_manager/
-    echo "Creating api_gateway image with port 9009"
+    echo
+
+    echo "#9 Creating api_gateway image with port 9009"
     docker build -t ag $ALCOR_ROOT_DIR/services/api_gateway/
-    echo "Creating data_plane_manager image with port 9010"
+    echo
+
+    echo "#10 Creating data_plane_manager image with port 9010"
     docker build -t dpm $ALCOR_ROOT_DIR/services/data_plane_manager/
-    echo "Creating elastic_ip_manager image with port 9011"
+    echo
+
+    echo "#11 Creating elastic_ip_manager image with port 9011"
     docker build -t eim $ALCOR_ROOT_DIR/services/elastic_ip_manager/
-    echo "Creating quoto_manager image with port 9012"
+    echo
+
+    echo "#12 Creating quoto_manager image with port 9012"
     docker build -t qm $ALCOR_ROOT_DIR/services/quota_manager/
-    echo "Creating network_acl_manager image with port 9013"
+    echo
+
+    echo "#13 Creating network_acl_manager image with port 9013"
     docker build -t nam $ALCOR_ROOT_DIR/services/network_acl_manager/
-    echo "Creating gateway_manager image with port 9015"
+    echo
+
+    echo "#14 Creating network_acl_manager image with port 9014"
+    docker build -t ncm $ALCOR_ROOT_DIR/services/network_config_manager/
+    echo
+
+    echo "#15 Creating gateway_manager image with port 9015"
     docker build -t gm $ALCOR_ROOT_DIR/services/gateway_manager/
+    echo
+
 }
 
 #Function stops and removes and then starts alcor container services
@@ -52,62 +86,124 @@ function stop_remove_start_alcor_containers()
 #Function to start alcor container services
 function start_alcor_containers()
 {
+    count=0
     echo "Starting Alcor services "
     docker run --name=ignite -p 10800:10800 -p 10801:10801 -p 47100:47100 -p 47500:47500 -v /tmp:/tmp -tid ignite-11 sh
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker run --net=host --name vpm -p 9001:9001 -v /tmp:/tmp -itd vpm
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker run --net=host --name snm -p 9002:9002 -v /tmp:/tmp -itd snm
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker run --net=host --name rm  -p 9003:9003 -v /tmp:/tmp -itd rm
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker run --net=host --name pim -p 9004:9004 -v /tmp:/tmp -itd pim
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker run --net=host --name mm  -p 9005:9005 -v /tmp:/tmp -itd mm
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker run --net=host --name pm  -p 9006:9006 -v /tmp:/tmp -itd pm
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker run --net=host --name nm  -p 9007:9007 -v /tmp:/tmp -itd nm
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker run --net=host --name sgm -p 9008:9008 -v /tmp:/tmp -itd sgm
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker run --net=host --name ag  -p 9009:9009 -v /tmp:/tmp -itd ag
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker run --net=host --name dpm -p 9010:9010 -v /tmp:/tmp -itd dpm
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker run --net=host --name eim -p 9011:9011 -v /tmp:/tmp -itd eim
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker run --net=host --name qm  -p 9012:9012 -v /tmp:/tmp -itd qm
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker run --net=host --name nam -p 9013:9013 -v /tmp:/tmp -itd nam
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
+    docker run --net=host --name ncm -p 9014:9014 -v /tmp:/tmp -itd ncm
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker run --net=host --name gm  -p 9015:9015 -v /tmp:/tmp -itd gm
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
+
+    echo -e "\n$count services started...\n"
 }
+
 # Function to stop alcor container services
 function stop_alcor_containers()
 {
+    count=0
     echo "Stopping all alcor services"
     docker container stop ignite
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker container stop vpm
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker container stop snm
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker container stop rm
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker container stop pim
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker container stop mm
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker container stop pm
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker container stop nm
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker container stop sgm
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker container stop ag
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker container stop dpm
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker container stop eim
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker container stop qm
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker container stop nam
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
+    docker container stop ncm
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker container stop gm
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
+
+    echo -e "\n$count services stopped...\n"
 }
+
 #Function to remove alcor containers
 function remove_alcor_containers()
 {
+    count=0
     echo "Removing Alcor containers"
     docker container rm ignite
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker container rm vpm
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker container rm rm
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker container rm pim
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker container rm qm
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker container rm nm
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker container rm mm
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker container rm sgm
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker container rm ag
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker container rm dpm
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker container rm eim
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker container rm pm
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker container rm snm
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker container rm nam
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
+    docker container rm ncm
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
     docker container rm gm
+    if [[ $? -eq 0 ]]; then count=$((count + 1)); fi
+
+    echo -e "\n$count services removed...\n"
 }
 
 function command_help()
---
+++ scripts/test-automation/busybox_helper_functions.py
@@ -0,0 +1,131 @@
+#!/usr/bin/python3
+import subprocess as sp
+from subprocess import *
+import sys
+import os
+
+# ok
+def make_docker_command(*argv):
+    command ='docker '
+    for arg in argv:
+      command += arg
+    return command
+
+def get_filelist(mypath):
+  print(mypath)
+  onlyfiles = os.listdir(mypath)
+  return onlyfiles
+
+def check_process_running(HOST, process):
+    running = False
+    COMMAND = 'ps -ef | grep -I {}'.format(process)
+    # print("In check process running", HOST, process)
+    output = run_command_on_remote(HOST, COMMAND)
+    if(output):
+      for line in output.decode(encoding='utf-8').split('\n'):
+        line = line.strip()
+        if not 'grep' in line:
+          # print("NOT A GREP LINE",line)
+          if(line):
+            running = True
+            return running
+
+
+def getmac_from_aca(HOST):
+    cmd = "ifconfig | grep -A 2 {} | tail -1".format(HOST)
+    output = run_command_on_remote(HOST, cmd)
+    addr_string = str(output).split()
+    mac_addr = addr_string[addr_string.index('ether')+1]
+    return mac_addr
+
+
+def run_command_on_remote(HOST, COMMAND):
+  try:
+     ssh1 = sp.Popen(['ssh', '-t', '{}@{}'.format('root', HOST), COMMAND], shell=False, stdout=sp.PIPE, stderr=sp.PIPE)
+     result = ssh1.communicate()
+     retcode = ssh1.returncode
+     # print(retcode)
+     if retcode > 0:
+       print("ERROR: ",result[1])
+       #return retcode
+     else:
+       return result[0]
+  except:
+     print("Exception Error:", sys.exc_info()[0])
+
+
+def run_command_forall(command, services_list):
+    status = True
+    # print("SSS", len(services_list))
+    for service in services_list:
+       # print("S ", service)
+       if(execute_command(command + service)):
+         status = False
+         #print("Failed to ",repr(str(command)))
+    return status
+
+
+def print_output(output):
+   for line in output.decode(encoding='utf-8').split('\n'):
+      line = line.strip()
+      print(line)
+
+
+def execute_command(command):
+  try:
+      pipe = sp.Popen(command,stdout=sp.PIPE,stderr=sp.PIPE,shell=True)
+      res = pipe.communicate()
+      retcode =pipe.returncode
+      #print(retcode)
+      if retcode >0:
+        print("Failed  to execute command", repr(str(command)))
+        #print("stderr =", res[1])
+        print_output(res[1])
+      else:
+        print("SUCCESS for command", command)
+        # print output of the command for debug purposes
+        # print_output(res[0])
+      return retcode
+  except:
+      print("ERROR", sys.exc_info()[0])
+
+
+def execute_commands(cmd, command_list):
+  print("execute commands ")
+  status = True
+  for command in command_list:
+    print(cmd, " ..", str(command))
+    if(execute_command(command)):
+     print("Failed to ", cmd, command)
+     status = False
+  return status
+
+
+def execute_checkcommand(command):
+    try:
+        print(command)
+        output = sp.check_output("ls /usr/bin/", stderr=sp.STDOUT, universal_newlines=True)
+        print(output)
+    except sp.CalledProcessError as error:
+        errorMessage = ">>> Error while executing:\n" + command + "\n>>> Returned with error:\n" + str(error.output)
+        print("ERROR: " + errorMessage)
+    except FileNotFoundError as error:
+         errorMessage = error.strerror
+         print("ERROR: ", errorMessage)
+
+#execute_command("mvn -Dmaven.test.skip=true -DskipTests clean package install")
+#execute_command("docker build -t ignite-11 -f/root/pingtest/alcor/lib/ignite.Dockerfile /root/pingtest/alcor/lib")
+#execute_command("ls /usr/bin")
+
+def dict_clean(dict):
+ result = {}
+ for key,value in dict:
+    if(value == 'null'):
+      value = 'None'
+    elif(value == 'true'):
+      value = 'True'
+    elif(value == 'false'):
+      value = 'False'
+    result[key]=value
+ return(result)
+
