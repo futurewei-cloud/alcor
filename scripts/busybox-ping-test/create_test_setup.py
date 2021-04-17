a9a160de5b3e11d308acf005c717bb3fc102bffa
Kiran <52575640+kiran1048@users.noreply.github.com>
2021-04-16 20:46:31 -0400

    [Test Script] Update busybox ping script to include two subnet test case (#585)

---
+++ scripts/busybox-ping-test/create_test_setup.py
@@ -0,0 +1,175 @@
+#!/usr/bin/python3
+
+#MIT License
+#Copyright(c) 2020 Futurewei Cloud
+#Permission is hereby granted, free of charge, to any person obtaining a copy 
+#of this software and associated documentation files(the "Software"), to deal 
+#in the Software without restriction, including without limitation the rights 
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+#copies of the Software, and to permit persons to whom the Software is 
+#furnished to do so, subject to the following conditions:
+#The above copyright notice and this permission notice shall be included in 
+#all copies or substantial portions of the Software.
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import requests
+import time
+import json
+from prepare_payload import *
+
+def create_default_segment_table(port):
+  print("Create default segment table")
+  url ='http://localhost:{}/segments/createDefaultTable'.format(port)
+  time.sleep(3)
+  post_httprequest(url)
+  print("SUCCESS: Created default segment table\n")
+
+
+def create_node(port, ip_mac):
+  print("Creating nodes")
+  url= 'http://localhost:{}/nodes'.format(port)
+  data = {}
+  node_info = read_config_file_section("node_info")
+  node_dict = node_info['node_info']
+  nodeinfo  = json.loads(node_dict)
+  node_name = nodeinfo['node_name']
+  node_id   = nodeinfo['node_id']
+
+  for key, value in ip_mac.items():
+    key_index = list(ip_mac).index(key) if key in ip_mac else None
+    node_info = {"local_ip":str(key), "mac_address":str(value), "node_id":node_id[key_index], "node_name":node_name[key_index], "server_port":nodeinfo['server_port'], "veth":nodeinfo['veth']}
+    data["host_info"] = node_info
+    post_httprequest(url, data)
+  print("SUCCESS: Created nodes\n")
+
+
+def create_router_interface(port):
+  print("Creating router interface")
+  router={}
+  url = 'http://localhost:{}/project/{}/routers'.format(port, get_projectid())
+  router_info = read_config_file_section("router_info")
+  router_dict = router_info['router_info']
+  routerinfo = json.loads(router_dict)
+  route_info = {"admin_state_up": True,"availability_zone_hints": ["string"], "availability_zones": ["string"],"conntrack_helpers": ["string"],"description": "string","distributed": True,"external_gateway_info": {"enable_snat": True,"external_fixed_ips": [ ],"network_id": routerinfo['network_id']},"flavor_id": "string","gateway_ports": [ ], "ha": True,"id":routerinfo['id'] ,"name": routerinfo['name'],"owner": routerinfo['owner'], "project_id":routerinfo['project_id'],"revision_number": 0,"routetable": {},"service_type_id": "string","status": "BUILD","tags": ["string"],"tenant_id": routerinfo['tenant_id']}
+  router['router'] = route_info
+  post_httprequest(url, router)
+  print("SUCCESS: Created router interface\n")
+  return routerinfo['id']
+
+# Second parameter is to indicate if the call is made for base test case or any other test case.
+def create_vpc(port, change={}):
+  print("Creating VPC")
+  network = {}
+  url = 'http://localhost:{}/project/{}/vpcs'.format(port, get_projectid())
+  network_info = read_config_file_section("vpc_info")
+  network_dict = network_info['vpc_info']
+  networkinfo = json.loads(network_dict)
+  if('change' in change):
+     networkinfo[change['change']] = change[change['change']]
+  network_info = {"admin_state_up":True, "revision_number":0, "cidr":networkinfo['cidr'], "default":True, "description":"vpc", "dns_domain":"domain", "id":networkinfo['id'], "is_default":True, "mtu":1400, "name":"sample_vpc", "port_security_enabled":True, "project_id":networkinfo['project_id']}
+  print(network_info)
+  network["network"] = network_info
+  post_httprequest(url, network)
+  print("SUCCESS: Created VPC\n")
+
+
+def create_subnet(port):
+  print("Creating Subnet")
+  subnet = {}
+  url = 'http://localhost:{}/project/{}/subnets'.format(port, get_projectid())
+  subnet_info = read_config_file_section("subnet_info")
+  subnetinfo = json.loads(subnet_info['subnet_info'])
+  subnet["subnet"] = subnetinfo
+  print("Posting subnet", subnet)
+  post_httprequest(url, subnet)
+  print("Creating Subnet\n")
+
+
+def create_security_group(port):
+  print("Creating security group")
+  security_groups = {}
+  url = 'http://localhost:{}/project/{}/security-groups'.format(port, get_projectid())
+  sg_info = read_config_file_section("security_groups")
+  sginfo = json.loads(sg_info['security_group_info'])
+  security_groups["security_group"]=sginfo
+  post_httprequest(url,security_groups)
+  print("SUCCESS: Created security group\n")
+
+
+def get_subnets(port):
+  url = 'http://localhost:{}/project/{}/subnets'.format(port, get_projectid())
+  print(get_httprequest(url))
+
+
+def get_nodes(port):
+  url= 'http://localhost:{}/nodes'.format(port)
+  print(get_httprequest(url))
+
+
+def get_vpcs(port):
+  url= 'http://localhost:{}/project/{}/vpcs'.format(port,get_projectid())
+  print(get_httprequest(url))
+
+
+def get_ports(port):
+  url= 'http://localhost:{}/project/{}/ports'.format(port,get_projectid())
+  print(get_httprequest(url))
+
+
+def create_ports(port):
+  print("Creating Goal State Ports")
+  url= 'http://localhost:{}/project/{}/ports'.format(port, get_projectid())
+  port_info = read_config_file_section("port_info")
+  port_dict = port_info['port_info']
+  port_dict = json.loads(port_dict, strict=False)
+  port_name = port_dict['name']
+  port_id   = port_dict['id']
+  ip_addrs  = port_dict['fixed_ips']
+  node_name = port_dict['binding:host_id']
+  device_id = port_dict['device_id']
+
+  for index in range(len(ip_addrs)):
+    ports = {}
+    port_info = {"admin_state_up":True,"allowed_address_pairs":[{"ip_address":"11.11.11.1","mac_address":"00-AA-BB-15-EB-3F"}],"binding:host_id":node_name[index],"binding:vif_details":{},"create_at":"string","description":"string","device_id":device_id[index],"device_owner":"compute:nova","dns_assignment":{},"dns_domain":"string","dns_name":"string","extra_dhcp_opts":[{"ip_version":"string","opt_name":"string","opt_value":"string"}],"fast_path": True,"fixed_ips":[{"ip_address":ip_addrs[index],"subnet_id":port_dict['subnet_id']}],"id":port_id[index],"mac_learning_enabled":True,"name":port_name[index],"network_id":port_dict['network_id'],"network_ns": "string","port_security_enabled":True,"project_id":port_dict['project_id'],"qos_network_policy_id":"string","qos_policy_id":"string","revision_number":0,"security_groups":[port_dict['security_groups']],"tags":["string"],"tenant_id":port_dict['tenant_id'],"update_at":"string","uplink_status_propagation":True,"veth_name":"string"}
+    ports["port"] = port_info
+    post_httprequest(url, ports)
+    print("after AAA posting  ports")
+
+  print("SUCCESS: Created Goal State Ports\n")
+
+
+def create_test_setup(ip_mac, config_file_object):
+  print("Calling Alcor APIs to generate Goal States")
+  services = dict(config_file_object.items("services"))
+  service_port_map = get_service_port_map(services)
+
+  create_default_segment_table(service_port_map["vpm"])
+
+  #network_info = dict(config_file_object.items("vpc_info"))
+  create_vpc(service_port_map["vpm"])
+  #get_vpcs(service_port_map["vpm"])
+
+  #node_info = dict(config_file_object.items("node_info"))
+  create_node(service_port_map["nm"], ip_mac)
+  #get_nodes(service_port_map["nm"])
+
+  #subnet_info = dict(config_file_object.items("subnet_info"))
+  create_subnet(service_port_map["snm"])
+  #get_subnets(service_port_map["snm"])
+
+  #sg_info = dict(config_file_object.items("security_groups"))
+  create_security_group(service_port_map["sgm"])
+
+  #port_info = dict(config_file_object.items("port_info"))
+  create_ports(service_port_map["pm"])
+  #get_ports(service_port_map["pm"])
+
+  ip_mac_db = get_mac_from_db()
+  print("Goal State IP/MACs: ", ip_mac_db)
+  return ip_mac_db
+
---
+++ scripts/busybox-ping-test/ping_test.py
@@ -0,0 +1,144 @@
+#!/usr/bin/python3
+
+#MIT License
+#Copyright(c) 2020 Futurewei Cloud
+#Permission is hereby granted, free of charge, to any person obtaining a copy 
+#of this software and associated documentation files(the "Software"), to deal 
+#in the Software without restriction, including without limitation the rights 
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+#copies of the Software, and to permit persons to whom the Software is 
+#furnished to do so, subject to the following conditions:
+#The above copyright notice and this permission notice shall be included in 
+#all copies or substantial portions of the Software.
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import time, os
+import argparse
+import json
+from helper_functions import *
+from create_test_setup import *
+from container_ops import *
+from create_test_cases import *
+
+ALCOR_ROOT = os.path.abspath(os.path.join(__file__ , "../../../"))
+ALCOR_SERVICES = ALCOR_ROOT + "/services/"
+ALCOR_TEST_DIR = os.path.dirname(os.path.abspath(__file__))
+os.chdir("../../")
+
+# Builds the containers as configured in alcor_services.ini file
+def build_containers(serv):
+    container_list =[]
+    mvn_build = "mvn -Dmaven.test.skip=true -DskipTests clean package install"
+    container_list.append(mvn_build)
+
+    print("building container images")
+    services_list = get_file_list(ALCOR_SERVICES)
+    for service_name in serv.keys():
+      service_path = ALCOR_SERVICES + service_name
+      service_info = json.loads(serv[service_name])
+      if service_name == "ignite":
+         build_cmd = make_docker_command("build ", " -t {}".format(service_info["name"]), " -f {} ".format(ALCOR_ROOT + service_info["path"]), ALCOR_ROOT + "/lib")
+      else:
+         build_cmd = make_docker_command("build ", "-t {} {}".format(service_info["name"], ALCOR_SERVICES + service_name))
+      container_list.append(build_cmd)
+
+    if(execute_commands("Build ", container_list) == True):
+       print("All Alcor services built successfully")
+
+
+def start_containers(serv):
+    start_containers= []
+    mount_option = "/tmp:/tmp"
+    for service_name in serv.keys():
+      service_info = json.loads(serv[service_name])
+      if service_name == "ignite":
+        start_cmd = make_docker_command("run ", "--name={} -p 10800:10800 -p 10801:10801 -p 47100:47100 -p 47500:47500 -v {} -tid {} sh".format(service_info["name"], mount_option, service_info["name"]))
+      else:
+        start_cmd = make_docker_command("run ", "--net=host ", "--name {} -p {}:{} -v {} -itd {}".format(service_info["name"], service_info["port"], service_info["port"],mount_option, service_info["name"]))
+      start_containers.append(start_cmd)
+
+    if(execute_commands("Start ", start_containers) == True):
+      print("All Alcor services started successfully")
+      return True
+    else:
+      print("Couldn not start all alcor services.")
+      print("Error! Test Exits")
+      sys.exit(1)
+
+
+def stop_containers(service_list):
+    command = "docker container stop "
+    for service in service_list:
+      execute_command(command + service)
+
+
+def remove_containers(service_list):
+    command = "docker container rm "
+    for service in service_list:
+      execute_command(command + service)
+
+
+def main():
+    config_file_object = read_config_file()
+    services = dict(config_file_object.items("services"))
+    service_port_map = get_service_port_map(services)
+    parser = argparse.ArgumentParser(description='Busybox ping test', epilog='Example of use: python script_name -b')
+    parser.add_argument("-b", "--build", type=str, nargs='?', help=' to build alcor services provide :{} as an option'.format('-b build'))
+    parser.add_argument("-t", "--testcase", type=int, nargs='?', help='Test case number or {} for all tests cases '.format('all'))
+    parser.add_argument("-s", "--all", type=str, nargs='?', help = 'all tests cases')
+    args = parser.parse_args()
+    """
+    if args.build:
+        if(args.build == "build"):
+           build_containers(services)
+        else:
+           print("invoke  as {}".format('-b build'))
+
+    stop_containers(service_port_map.keys())
+    remove_containers(service_port_map.keys())
+    if(start_containers(services) == True):
+      print("All services started Sucessfully")
+    else:
+      print("Error:couldn't start all alcor services")
+      sys.exit(1)
+    """
+    aca = dict(config_file_object.items("AlcorControlAgents"))
+    aca_nodes_ip_mac = check_alcor_agents_running(aca)
+    if(len(aca_nodes_ip_mac) != len(aca)):
+      print("\nERROR: Alcor Control Agent not running on some of the nodes")
+      print("ERROR: Test exits")
+      sys.exit(1)
+    print("Wait for 20 seconds until all services are started...")
+    #time.sleep(20)
+
+    if args.testcase:
+      if (args.testcase == 1):
+        ip_mac_db = prepare_test_case_1(aca_nodes_ip_mac, service_port_map)
+      elif(args.testcase == 2):
+        ip_mac_db = prepare_test_case_2(aca_nodes_ip_mac, service_port_map)
+      else:
+        print("Invoke {}".format('-t <testcase number>'))
+    else:
+      ip_mac_db = create_test_setup(aca_nodes_ip_mac, config_file_object)
+      # if args.all== 'all':
+      #  print("Invoke both all test cases"
+      # ip_mac_db = prepare_all_test_cases(aca_nodes_ip_mac, service_port_map)'''
+
+    #aca_nodes_ip_mac={"10.213.43.161":"90:17:ac:c1:30:68", "10.213.43.163":"90:17:ac:c1:30:3c"}
+    #ip_mac_db={"10.0.1.101":"aa:bb:cc:a8:c9:c6", "10.0.1.102":"aa:bb:cc:df:79:f1"}
+    container_names_dict = dict(config_file_object.items("test_setup"))["container_names"]
+    container_names = json.loads(container_names_dict)
+    print(container_names)
+    #container_names = ["con1", "con2"]
+    print("calling container deploy", aca_nodes_ip_mac, ip_mac_db, container_names)
+    busybox_container_deploy(list(aca_nodes_ip_mac.keys()), ip_mac_db, container_names)
+
+
+if __name__ == "__main__":
+    main()
+
