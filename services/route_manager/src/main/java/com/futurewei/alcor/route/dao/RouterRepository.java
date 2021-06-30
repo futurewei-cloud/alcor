32dd4afd885c4a27bf9f96198950b8d68371dc43
Yan Mo <andymo96@gmail.com>
2021-06-30 15:41:54 -0700

    remove try catch to all file under dao

--- services/route_manager/src/main/java/com/futurewei/alcor/route/dao/RouterRepository.java
+++ services/route_manager/src/main/java/com/futurewei/alcor/route/dao/RouterRepository.java
@@ -75,15 +75,7 @@ public class RouterRepository implements ICacheRepository<Router> {
     @Override
     @DurationStatistics
     public void addItem(Router router) throws CacheException {
-        try (Transaction tx = cache.getTransaction().start()) {
-
-            logger.log(Level.INFO, "Add router, router Id:" + router.getId());
-            cache.put(router.getId(), router);
-
-            tx.commit();
-        } catch (Exception e) {
-            throw new CacheException();
-        }
+        cache.put(router.getId(), router);
     }
 
     @Override
