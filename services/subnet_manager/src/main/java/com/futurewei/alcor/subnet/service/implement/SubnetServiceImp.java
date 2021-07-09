ad62ef0dc3222fe47bb22f75e24b3c2dd49693f1
Liguang Xie <lxie@futurewei.com>
2021-07-09 11:22:57 -0700

    Add some logs

--- services/subnet_manager/src/main/java/com/futurewei/alcor/subnet/service/implement/SubnetServiceImp.java
+++ services/subnet_manager/src/main/java/com/futurewei/alcor/subnet/service/implement/SubnetServiceImp.java
@@ -313,8 +313,10 @@ public class SubnetServiceImp implements SubnetService {
         String portManagerServiceUrl = portUrl + "project/" + projectId + "/subnet-port-count/" + subnetEntity.getId();
         int portCount = restTemplate.getForObject(portManagerServiceUrl, Integer.class);
         if (portCount == 0 && StringUtil.isNullOrEmpty(subnetEntity.getGatewayPortId())) {
+            logger.info("portCount == 0 && subnetEntity.getGatewayPortId() = " + subnetEntity.getGatewayPortId());
             return false;
         } else if (portCount == 1 && !StringUtil.isNullOrEmpty(subnetEntity.getGatewayPortId())) {
+            logger.info("portCount == 1 && subnetEntity.getGatewayPortId() = " + subnetEntity.getGatewayPortId());
             return false;
         }
 
