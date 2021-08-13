f4df13414936b33f6f85b1cac1cdafd0b9657e88
Rio Zhu <zzhu@futurewei.com>
2021-08-13 12:36:46 -0700

    Sending only gsv1, to see if it works

--- services/pseudo_controller/src/main/java/com/futurewei/alcor/pseudo_controller/pseudo_controller.java
+++ services/pseudo_controller/src/main/java/com/futurewei/alcor/pseudo_controller/pseudo_controller.java
@@ -278,8 +278,8 @@ public class pseudo_controller {
         System.out.println("Created GoalStateOperationReply observer class");
         io.grpc.stub.StreamObserver<Goalstate.GoalStateV2> response_observer = stub.pushGoalStatesStream(message_observer);
         System.out.println("Connected the observers");
-        response_observer.onNext(message_one);
-        response_observer.onNext(message_two);
+//        response_observer.onNext(message_one);
+//        response_observer.onNext(message_two);
 
         System.out.println("After calling onNext");
         response_observer.onCompleted();
