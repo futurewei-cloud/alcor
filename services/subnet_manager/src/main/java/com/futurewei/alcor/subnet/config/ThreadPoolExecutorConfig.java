54185bfc9d49a683ce6b8a5c552d98aff2c1a0b4
Dahai Liu <dliu@futurewei.com>
2021-10-05 18:54:57 -0700

    Update code

--- services/subnet_manager/src/main/java/com/futurewei/alcor/subnet/config/ThreadPoolExecutorConfig.java
+++ services/subnet_manager/src/main/java/com/futurewei/alcor/subnet/config/ThreadPoolExecutorConfig.java
@@ -17,7 +17,7 @@ package com.futurewei.alcor.subnet.config;
 
 public class ThreadPoolExecutorConfig {
 
-    public static int corePoolSize = 64;
+    public static int corePoolSize = 20;
     public static int maximumPoolSize = 20;
     public static int KeepAliveTime = 5000;
     public static int capacity = 1024;
