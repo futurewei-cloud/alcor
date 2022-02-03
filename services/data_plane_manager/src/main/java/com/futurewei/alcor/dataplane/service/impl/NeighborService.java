12dc66dd307c35f89c78471c5236ffcaad66a868
Dahai Liu <dliu@futurewei.com>
2022-02-03 14:02:38 -0800

    Update code

--- services/data_plane_manager/src/main/java/com/futurewei/alcor/dataplane/service/impl/NeighborService.java
+++ services/data_plane_manager/src/main/java/com/futurewei/alcor/dataplane/service/impl/NeighborService.java
@@ -451,8 +451,10 @@ public class NeighborService extends ResourceService {
                 e.printStackTrace();
             }
         });
+        unicastHostResourceBuilder.addAllResources(unicastGoalStateV2.getGoalStateBuilder().getHostResourcesMap().get(unicastGoalStateV2.getHostIp()).getResourcesList());
         unicastGoalStateV2.getGoalStateBuilder().putHostResources(unicastGoalStateV2.getHostIp(), unicastHostResourceBuilder.build());
         for (String hostIp : multicastGoalStateV2.getHostIps()) {
+            multicastHostResourceBuilder.addAllResources(multicastGoalStateV2.getGoalStateBuilder().getHostResourcesMap().get(hostIp).getResourcesList());
             multicastGoalStateV2.getGoalStateBuilder().putHostResources(hostIp, multicastHostResourceBuilder.build());
         }
         unicastGoalStateV2.getGoalStateBuilder().putAllNeighborStates(neighborStateMap);
