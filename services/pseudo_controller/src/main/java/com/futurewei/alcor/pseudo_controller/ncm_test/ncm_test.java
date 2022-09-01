4cc47db8775b878a69a723919d934d1ee8be7b0f
Rio Zhu <zzhu@futurewei.com>
2022-09-01 16:01:48 -0700

    Use separate threads to run ArionAgent

--- services/pseudo_controller/src/main/java/com/futurewei/alcor/pseudo_controller/ncm_test/ncm_test.java
+++ services/pseudo_controller/src/main/java/com/futurewei/alcor/pseudo_controller/ncm_test/ncm_test.java
@@ -616,12 +616,15 @@ public class ncm_test {
                     String current_time = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new java.util.Date());
                     for (int i = 0 ; i < nodes.size() ; i ++) {
                         JSONObject current_node = (JSONObject) nodes.get(i);
-                        String start_arion_agent_ssh_command = "sudo nohup " + (String) current_node.get("arion_agent_location") +
-                                " -d -a " + arion_address_without_http + " -p " + arion_master_grpc_port + " -g " + DEFAULT_ARION_GROUP_NAME +
-                                " > /tmp/ArionAgent.log 2>&1 &";
+                        String start_arion_agent_ssh_command = "sudo " + (String) current_node.get("arion_agent_location") +
+                                " -d -a " + arion_address_without_http + " -p " + arion_master_grpc_port + " -g " + DEFAULT_ARION_GROUP_NAME
+//                                + " > /tmp/ArionAgent.log 2>&1 &"
+                                ;
                         Vector<String> cmds = new Vector<>();
                         cmds.add(start_arion_agent_ssh_command);
-                        execute_ssh_commands(cmds, (String) current_node.get("ip_control"), (String) current_node.get("id_control"), (String) current_node.get("pwd_control"));
+                        new Thread(() -> {
+                            execute_ssh_commands(cmds, (String) current_node.get("ip_control"), (String) current_node.get("id_control"), (String) current_node.get("pwd_control"));
+                        }).start();
                     }
                     try {
                         TimeUnit.SECONDS.sleep(30);
